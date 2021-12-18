7f1772a99456a48a47338cf2353a2aaa5a4cdb7a
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2021-12-18 19:12:50 +0800

    已完成讀txt檔並設為LIST，現卡在不確定要找哪個POS tagging

--- VOfinding.py
+++ VOfinding.py
@@ -16,3 +16,5 @@ if __name__ == "__main__":
                         tmpLIST.extend(k)
                 fileLIST = tmpLIST
                 pprint(fileLIST)
+                
+                # SHA LALA
