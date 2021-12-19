90d6bd980bfc68413ed07205729f9470daab6bf0
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2021-12-19 13:29:03 +0800

    新增去除空格的指令，還在確認到底要找哪個POStagging

--- VOfinding.py
+++ VOfinding.py
@@ -9,12 +9,19 @@ if __name__ == "__main__":
         
         data_path = "/Users/neuroling/Datasets/中研院平衡語料庫/00_documents_only/"
         tmpLIST = []
-        with open (data_path + "txt_001_40.txt", "r", encoding = "utf-8") as raw_file:   
+        with open (data_path + "txt_001_{}.txt".format(41), "r", encoding = "utf-8") as raw_file:   
                 fileLIST = raw_file.read().split("\n")
                 for big_item in fileLIST:
-                        k = big_item.split("\u3000")
+                        k = big_item.split("\u3000")    
                         tmpLIST.extend(k)
-                fileLIST = tmpLIST
+                                
+                        fileLIST = tmpLIST
+                #pprint(fileLIST)        
+                for i in fileLIST:
+                        if len(i) < 1:
+                                print([i])
+                                fileLIST.remove(i)
+                        else:
+                                pass
                 pprint(fileLIST)
-                
-                # SHA LALA
+                pprint(len(fileLIST))
