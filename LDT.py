fdc91811593ce0d643acc8c7f900276b49e4dc2b
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-01-18 10:52:50 +0800

    新增了幾道指導語

--- LDT.py
+++ LDT.py
@@ -16,38 +16,55 @@ if __name__ == "__main__":
     # Step_1: load in all the stimuli to start the LDT.py, and show the instructions
     
     win = visual.Window(size = [500, 500], units ="pix")
-    instructions = visual.TextStim(win = win, text = "接下來你會看到一串數字\n，請依照實驗指示進行按鍵反應\n，當你準備好的時候\n，請按下空白鍵\n")  # 這裡中文字顯現的有點奇怪
+    instructions_0 = visual.TextStim(win = win, text = "接下來你會看到一串數字\n，請依照實驗指示進行按鍵反應\n，當你準備好的時候\n，請按下空白鍵\n")  # 這裡中文字顯現的有點奇怪
     fixations = visual.TextStim(win = win, text = "+")
     testing_stimuli = visual.TextStim(win = win, text = "word")
     resultLIST = []  # for contain the response resultLIST
     
     # for showing the instructions, and instruct them to press key to continue
-    instructions.draw()
+    instructions_0.draw()
     win.flip()
     event.waitKeys(keyList = ['space'])  # 沒有keylist的話，按任何按鍵都會跳到下一張  # 指定按鍵才可以跳到下一張，在這裡指的就是空白鍵
+    
+    instructions_1 = visual.TextStim(win = win, text = "真詞按z 假詞按/\n請按任意鍵繼續")  # 這裡中文字顯現的有點奇怪
+    instructions_1.draw()
+    win.flip()
+    event.waitKeys()
+    
+    
+    instructions_2 = visual.TextStim(win = win, text = "將你的左食指輕放在z鍵，右食指輕放在/鍵。\n請按任意鍵繼續")  # 這裡中文字顯現的有點奇怪
+    instructions_2.draw()
+    win.flip()
+    event.waitKeys()       
+    
+    instructions_3 = visual.TextStim(win = win, text = "當字詞出現時，請盡快且正確的進行按鍵反應。\n請按任意鍵繼續")  # 這裡中文字顯現的有點奇怪
+    instructions_3.draw()
+    win.flip()
+    event.waitKeys(keyList = ['z', '/'])
     
     # Step_2: show the cross in the central of the screen
     # for showing the fixation ["+"]
     fixations.draw()
     win.flip()
-    core.wait(5)
+    core.wait(3)
     
     # Step_3: filp to a blank screen
-    win.flip()
-    
+    win.flip()    
     
     # Step_4: show the stimuli(real words or pseudowords), and remain the stimuli for 400ms
     testing_stimuli.draw()
     win.flip()
-    event.waitKeys()
+    event.waitKeys(keyList = ['space'])
+    
+    """
     resultLIST = event.getKeys(keyList = None, modifiers = True, timeStamped = True)
     
-    with open('/Users/ting-hsin/Downloads/LDT-testing-resultLIST.json', "w", newline='', encoding="UTF-8") as jsonfile:
+    with open('/Users/neuroling/Documents/ICN_related-main/LDT-testing-resultLIST.json', "w", newline='', encoding="UTF-8") as jsonfile:
         json.dump(resultLIST, jsonfile, ensure_ascii=False)
         
     
     core.wait(3)
-    
+    """
     # Step_5: remove the stimuli, and then show the blank screen for 1500ms (waiting for the participants to react)
     
     # Step_6: if the participanst react, then record the answer and the reaction time that were given by the participant, if not, then record a blank in the results 
