348d09ba8196bb71eaed7e76033f0b44b1ba35d5
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-02-11 15:02:32 +0800

    修改.getKey()指令及增加幾項LDT註解

--- LDT.py
+++ LDT.py
@@ -65,40 +65,20 @@ if __name__ == "__main__":
         testing_stimuli.draw()
         win.flip()
         #core.wait(2) #DON'T NEED THIS IN HERE! #the waiting time need to rethink about it, cause something is not right
-        event.waitKeys() #keyList = ['space'])
-        keys = event.getKeys()
+        event.waitKeys(keyList = ["z", "slash"])
+        keys = event.getKeys(keyList = ["z", "slash"], modifiers = True, timeStamped = False) # 再加上if else的判斷決定是否要收或是要怎麼紀錄這反應
+        """
+        # 按鍵反應 + 按鍵時間（RT）+ (trigger_for 腦波) + 正確與否
+        # basic info = trails數（的第幾題）,trai_list(第幾次trial), sub_num, date, duration
+        # added these two
+        # clock.getTime()
+        # clock.reset()
+        """
+        
         tmpLIST.append(keys)
     resultKeyLIST = tmpLIST
-        
-    
-    """
-    kb = keyboard.Keyboard()
-
-    # during your trial
-    kb.clock.reset()  # when you want to start the timer from
-    keys = kb.getKeys(['z', 'right', 'quit'], waitRelease=True)
-    if 'quit' in keys:
-        core.quit()
-    for key in keys:
-        print(key.name, key.rt, key.duration)
-    #"""
     #keys = event.getKeys(keyList = ['z', '/'], modifiers = False, timeStamped = False)
     
-    #core.wait(3)
-    
-    
-    """
-    # the function for getting the keys
-    kb = keyboard.Keyboard()
-
-    # during your trial
-    kb.clock.reset()  # when you want to start the timer from
-    keys = kb.getKeys(['right', 'left', 'quit'], waitRelease=True)
-    if 'quit' in keys:
-    core.quit()
-    for key in keys:
-    print(key.name, key.rt, key.duration)
-    """
     #event.waitKeys(keyList = ['space'])
     
     
