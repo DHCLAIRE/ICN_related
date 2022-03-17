4d3fecf442bb04db5e8587e650ee46581b064fe9
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-03-17 11:44:20 +0800

    新增每層字母的篩選指令

--- LTTC_pseudowords.py
+++ LTTC_pseudowords.py
@@ -5,11 +5,16 @@ from pprint import pprint
 import csv
 import json
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+	
 	data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/二代語料/"
 	tmpLIST = []
 	tmpLIST_2 = []
+	tmpLIST_3 = []
+	tmpLIST_4 = []
+	tmpLIST_5 = []
+	tmpLIST_6 = []
 	raw_pseudowordsLIST = []
 	
 	
@@ -19,31 +24,66 @@ if __name__ == "__main__":
 		
 		for pseudo_item in fileLIST:
 			tmpLIST = pseudo_item.split(",")
-			if len(tmpLIST[0]) == 6:
-				print(tmpLIST[0])
+			if len(tmpLIST[0]) == 7:
+				#print(tmpLIST[0])
 				raw_pseudowordsLIST.extend([tmpLIST[0]])
 			else:
 				pass
-
-		for lvl2_pseudoItem in raw_pseudowordsLIST:
-			print(lvl2_pseudoItem)
-			print(type(lvl2_pseudoItem))
 			
-			if 'b' in lvl2_pseudoItem:
+			
+		# the 1st layer
+		for lvl2_pseudoItem in raw_pseudowordsLIST:
+			#print(lvl2_pseudoItem)
+			#print(type(lvl2_pseudoItem))
+			
+			if '' in lvl2_pseudoItem:
 				print(lvl2_pseudoItem)
 				tmpLIST_2.extend([lvl2_pseudoItem])
-				if 'c' in lvl2_pseudoItem:
-					print(lvl2_pseudoItem)
-					tmpLIST_2.extend([lvl2_pseudoItem])				
-								
-			#if 'e' in lvl2_pseudoItem:
-				#print(lvl2_pseudoItem)
-				#tmpLIST_2.extend([lvl2_pseudoItem])				
-			#if 'c' in lvl2_pseudoItem:
-				#print(lvl2_pseudoItem)
-				#tmpLIST_2.extend([lvl2_pseudoItem])				
-			
 			else:
 				pass
 		print(tmpLIST_2)
 		print(len(tmpLIST_2))
+		"""
+		# the 2nd layer
+		for c in tmpLIST_2:
+			if 'e' in c:
+				print(c)
+				tmpLIST_3.extend([c])
+			else:
+				pass
+		print(tmpLIST_3)
+		print(len(tmpLIST_3))
+		
+		
+		# the 3rd layer
+		for z in tmpLIST_3:
+			if 's' in z:
+				print(z)
+				tmpLIST_4.extend([z])
+			else:
+				pass
+		print(tmpLIST_4)
+		print(len(tmpLIST_4))
+		
+		
+		# the 4th layer
+		for a in tmpLIST_4:
+			if 't' in a:
+				print(a)
+				tmpLIST_5.extend([a])
+			else:
+				pass
+		print(tmpLIST_5)
+		print(len(tmpLIST_5))	
+		
+		
+		# the 5th layer
+		for b in tmpLIST_5:
+			if 'n' in b:
+				print(c)
+				tmpLIST_6.extend([b])
+			else:
+				pass
+		print(tmpLIST_6)
+		print(len(tmpLIST_6))			
+		"""
