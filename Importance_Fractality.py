7e3fc9cb688cec85d29f50bcc2f0a6d5df8c6599
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-03-28 17:03:55 +0800

    新增Importance的計算原理

---
+++ Importance_Fractality.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+"""
+Citation: Najafi, E., & Darooneh, A. H. (2017). Long range dependence in texts: A method for quantifying coherence of text. Knowledge-Based Systems, 133, 33-42.
+COPIED CONTENT FROM THE ARTICLE:
+
+The fractal dimension of words in this one-dimensional space is a number between 0 and 1.
+The important words have dimensions that are significantly different from one, while the less important words are distributed 
+uniformly and their dimensions are close to one [6].
+Box counting is a practical method for computing the fractal dimension of words.
+To perform this calculation, the text should be divided into boxes of size s.
+It means that any box contains s successive words.
+The number of such boxes that contains at least one occurrence of a given word w,
+
+i.e. the number of filled boxes, is Nb(w, s) and the self-similarity
+property is expressed as,
+
+Nb(w, s) ∼ s−Dw . (1)
+
+where Dw stands for the fractal dimension of word w.
+It is obtained by finding the slope of log-log plot of Nb(w, s) versus s for large enough s.
+
+"""
+
+'''
+The importance of Fractality:
+Calculation process:
+Box counting: t
+
+Box = the length of the text
+
+s = the size of the boxes = s words that contains in this box
+w = the target word  (??) I guess
+
+'''
+'''
+Steps:
+1. split the text into different lengths = create s??
+2. shuffle the texts??
+3. calculate the texts
+'''
+
+from pprint import pprint
+import csv
+import json
+
+
+
+if __name__ == "__main__":
+    
