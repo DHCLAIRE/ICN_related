86411e0e31de1e32b4ee90c433b82cd9a045af9e
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-03-28 19:55:33 +0800

    完成了隨機挑選6個目標假詞的指令

--- LTTC_stim-forming.py
+++ LTTC_stim-forming.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from pprint import pprint
-import csv
-import json
-
 '''
 Steps: 
-1. create a list that only have 12 pseudowords
-2. import the pseudoword list
-3. randomly select 6 out of the list of 12 pseudowords as the target words
+1. (DONE)create a list that only have 12 pseudowords
+2. (DONE)import the pseudoword list
+3. (DONE)randomly select 6 out of the list of 12 pseudowords as the target words
 4. select 3 out of the 6 target words and divided 3-3 into High-CD and Low-CD conditions
 5. import all the pre-selected bunch of texts
 6. randomly selelct a pair set of High-CD and Low-CD texts
@@ -17,48 +13,59 @@ Steps:
 8. 
 '''
 
+from pprint import pprint
+import csv
+import json
+import random
+from random import sample
+
 
 if __name__ == "__main__":
     data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/2nd_Stim-Materials/"
     tmpLIST = []
     tmpLIST_2 = []
     pseudoLIST = []
+    targetPseudoLIST = []
     
+    """
+    # 1_Create the pseudoword LIST and save it into json file
     
+    # Open the csv file that contains the info of 12 pseudowords
     with open (data_path + "2nd_Pseudowords_12.csv", "r", encoding = "utf-8") as raw_file:
         fileLIST = raw_file.read().split("\n")
         #pprint(fileLIST)
-        
+    
+    # Organized the csv into a more convenient form of structure, which is a list of lists that carry the info of each pseudoword
     for i in fileLIST:
         tmpLIST = i.split(",")
-        
-        #print(tmpLIST)
-        #print(type(tmpLIST))
         tmpLIST_2.append(tmpLIST)
-        
+    # exclude the head title in the csv file
     tmpLIST_2.pop(0)
-    
     print(tmpLIST_2)
     
+    # Extract the pseudowords from the organized list
     for k in tmpLIST_2:
         pseudoSTR = k[0]
         pseudoLIST.extend([pseudoSTR])
-        #print(k)
-        #print(type(k))
-        #print(pseudoSTR)
-        #print(type(pseudoSTR))
     print(pseudoLIST)
+    
+    # Save the pseudoword LIST into a json file
+    #with open(data_path + 'LTTC-pseudowordLIST.json', "w", newline='', encoding="UTF-8") as jsonfile:
+        #json.d ump(pseudoLIST, jsonfile, ensure_ascii=False)
+        """
+    
+    # 2_Import the pseudoword list (in json file form)
+    with open (data_path + "LTTC-pseudowordLIST.json", "r", encoding = "utf-8") as jfile:
+        pseudoLIST = json.load(jfile)
+        #print(pseudoLIST)
         
-    """
-        for z in tmpLIST:
-            if len(z) == 0:
-                tmpLIST.remove(z)
-                print(tmpLIST)
-            else:
-                pass 
-        tmpLIST_2.append(tmpLIST)
-    pprint(tmpLIST_2)
-    """
-    with open(data_path + 'LTTC-pseudowordLIST.json', "w", newline='', encoding="UTF-8") as jsonfile:
-        json.dump(pseudoLIST, jsonfile, ensure_ascii=False)
+        # suffle the pseudoword list (shuffle or not, both works)
+        random.shuffle(pseudoLIST)
+        print(pseudoLIST)
+        
+    # 3_Randomly select 6 out of the list of 12 pseudowords as the target words
+        
+        # randomly select 6 target pseudowords from the list
+        targetPseudoLIST = sample(pseudoLIST, 6)
+        print(targetPseudoLIST)
         
