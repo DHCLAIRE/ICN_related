3c41cd04d7631c76789952a854fe5669e6b68e48
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-03-30 20:22:25 +0800

    新增叫入文本的指令

--- LTTC_stim-forming.py
+++ LTTC_stim-forming.py
@@ -7,10 +7,10 @@ Steps:
 2. (DONE)import the pseudoword list
 3. (DONE)randomly select 6 out of the list of 12 pseudowords as the target words
 4. (DONE)select 3 out of the 6 target words and divided 3-3 into High-CD and Low-CD conditions
-5. import all the pre-selected bunch of texts
-6. randomly selelct a pair set of High-CD and Low-CD texts
-7. insert the assigned pseudowords into the pair set of High-CD and Low-CD texts
-8. 
+5. (DONE)import all the pre-selected bunch of texts
+6. divided all the pre-selected texts into the High-CD and Low-CD sets
+7. randomly selelct a pair set of High-CD and Low-CD texts
+8. insert the assigned pseudowords into the pair set of High-CD and Low-CD texts
 '''
 
 from pprint import pprint
@@ -21,20 +21,23 @@ from random import sample
 
 
 if __name__ == "__main__":
-    data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/2nd_Stim-Materials/"
+    stim_data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/2nd_Stim-Materials/"
+    text_data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/2nd_Stim-Materials/USE_Output/LTTC_modifiedTexts_output/"
     tmpLIST = []
     tmpLIST_2 = []
     pseudoLIST = []
     targetPseudoLIST = []
     controlPseudoLIST = []
-    high_CD_setLIST = []
-    low_CD_setLIST = []
+    words_high_CD_setLIST = []
+    words_low_CD_setLIST = []
+    texts_high_CD_setLIST = []
+    texts_low_CD_setLIST = []
     
     """
     # 1_Create the pseudoword LIST and save it into json file
     
     # Open the csv file that contains the info of 12 pseudowords
-    with open (data_path + "2nd_Pseudowords_12.csv", "r", encoding = "utf-8") as raw_file:
+    with open (stim_data_path + "2nd_Pseudowords_12.csv", "r", encoding = "utf-8") as raw_file:
         fileLIST = raw_file.read().split("\n")
         #pprint(fileLIST)
     
@@ -53,18 +56,14 @@ if __name__ == "__main__":
     print(pseudoLIST)
     
     # Save the pseudoword LIST into a json file
-    #with open(data_path + 'LTTC-pseudowordLIST.json', "w", newline='', encoding="UTF-8") as jsonfile:
+    #with open(stim_data_path + 'LTTC-pseudowordLIST.json', "w", newline='', encoding="UTF-8") as jsonfile:
         #json.d ump(pseudoLIST, jsonfile, ensure_ascii=False)
         """
     
     # 2_Import the pseudoword list (in json file form)
-    with open (data_path + "LTTC-pseudowordLIST.json", "r", encoding = "utf-8") as jfile:
+    with open (stim_data_path + "LTTC-pseudowordLIST.json", "r", encoding = "utf-8") as jfile:
         pseudoLIST = json.load(jfile)
         print("12 pseudowords = ", pseudoLIST)
-        
-        # suffle the pseudoword list (shuffle or not, both works)
-        #random.shuffle(pseudoLIST)
-        #print(pseudoLIST)
         
     # 3_Randomly select 6 out of the list of 12 pseudowords as the target words
         # randomly select 6 target pseudowords from the list
@@ -81,16 +80,32 @@ if __name__ == "__main__":
         print("The ControlPseudo words = ", controlPseudoLIST)
         
     # 4_Select 3 out of the 6 target words and divided 3-3 into High-CD and Low-CD conditions
-        high_CD_setLIST = sample(targetPseudoLIST, 3)
-        #print("High-CD_set = ", high_CD_setLIST)
+        words_high_CD_setLIST = sample(targetPseudoLIST, 3)
         
         for w in targetPseudoLIST:
-            if w not in high_CD_setLIST:
-                low_CD_setLIST.extend([w])
+            if w not in words_high_CD_setLIST:
+                words_low_CD_setLIST.extend([w])
             else:
                 pass
 
-        print("High-CD_set = ", high_CD_setLIST)
-        print("Low-CD_set = ", low_CD_setLIST)
+        print("High-CD_set = ", words_high_CD_setLIST)
+        print("Low-CD_set = ", words_low_CD_setLIST)
     
     # 5_Import all the pre-selected bunch of texts
+    
+    # High-CD sets 
+    # texts_high_CD_setLIST = [345, 456, 567, 367, 347]
+    
+    # Low-CD sets
+    # texts_low_CD_setLIST = [128, 289, 890, 190, 120]
+    
+    # reload the texts
+    with open (text_data_path + "LTTC-modifiedText_OneLIST.json", "r", encoding = "utf-8") as jfile_2:
+        textLIST = json.load(jfile_2)
+        
+        for text in textLIST:
+            text = [text]
+            pprint(text)
+            
+    # 6_divided all the pre-selected texts into the High-CD and Low-CD sets
+        
