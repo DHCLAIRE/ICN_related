4edb64233a6b1277a3bd8691f0748d614ef719e1
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-04-09 21:08:14 +0800

    新增含送trigger的指令(先註解掉)

--- Alice_SoundPlay.py
+++ Alice_SoundPlay.py
@@ -6,12 +6,13 @@ from psychopy import prefs
 prefs.hardware['audioLib'] = ['PTB', 'pyo', 'pygame']
 
 import psychtoolbox as ptb
-from psychopy import sound, core, visual   # if you change the setting, this command must be put after the prefs's command
+from psychopy import sound, core, visual, event, gui, monitors, clock  #, parallel   # if you change the setting, this command must be put after the prefs's command
 #import json
 print(sound.Sound)
 
 import scipy
 from scipy.io import wavfile
+import numpy as np
 
 """
 # function to convert the information into
@@ -25,7 +26,7 @@ def output_duration(length):
 
     return hours, mins, seconds
 """
-"""
+
 # 想想要如何同時撥出十字和音檔 >> or not
 def display_fix():
     '''
@@ -34,7 +35,22 @@ def display_fix():
     fixation = visual.TextStim(win = win, text = "+")
     fixation.draw()
     win.flip()
+    
 """
+# For setting up the Trigger
+n = 0
+port = 'COM3'
+baudRate = 115200
+InputBufferSize = 8
+readTimeout = 1
+portSettings = 'BaudRate=%d InputBufferSize=%d Terminator=0 ReceiveTimeout=%f ReceiveLatency=0.0001'%(
+    baudRate, InputBufferSize, readTimeout)
+
+[handle, errmsg] = ptb.IOPort('OpenSerialPort', port, portSettings)
+ptb.IOPort('Flush', handle)
+"""
+win = visual.Window(color = [-1, -1, -1], units ="pix", fullscr = True)   # Present screen_Full
+
 
 if __name__ == "__main__":
     data_path = "/Volumes/Neurolang_1/Master Program/New_Thesis_topic/Alice(EEG dataset and stimuli)/audio/"
@@ -43,6 +59,9 @@ if __name__ == "__main__":
     # in (sample/sec) format
     # data is the numpy array that consists
     # of actual data read from the wav file
+    
+    # display fixation
+    display_fix()
     
     for i in range(12):
         
@@ -57,13 +76,32 @@ if __name__ == "__main__":
         Alice_stm = data_path + "DownTheRabbitHoleFinal_SoundFile{}.wav".format(i+1)
         Script_Sound = sound.Sound(Alice_stm)   #value=str(Alice_stm), secs = 60)
         #now = ptb.GetSecs()
-        Script_Sound.play() 
+        Script_Sound.play()
         core.wait(int(t+1))  # switch this num into the length of each audio files
+        """
+        # TO MARK THE AUDIO FILE BEGINS  # This is the trigger_marker for marking the start of the audio file
+        ptb.IOPort('Write', handle, np.uint8([109,104,np.uint8(int(i+1)),np.uint8(0)]))  #This is open the trigger
+        core.wait(0.01) # Stay for 10 ms
+        ptb.IOPort('Write', handle, np.uint8([109,104,np.uint8(0),np.uint8(0)])) #This is close the trigger
+        """
+        
         print("SoundFile{}".format(i+1), "DONE")
         print("Pause for 5 seconds.")
+        
+        """
+        # TO MARK THE AUDIO FILE ENDS
+        ptb.IOPort('Write', handle, np.uint8([109,104,np.uint8(99),np.uint8(0)]))  #This is open the trigger
+        core.wait(0.01) # Stay for 10 ms
+        ptb.IOPort('Write', handle, np.uint8([109,104,np.uint8(0),np.uint8(0)])) #This is close the trigger
+        """
+        
         # the Gap between each audio files
         core.wait(5)
         print("Continue for the SoundFile{}".format(i+2))
+        
+        
+        # Add ESC could core.quit() function in the middle of the experiments process
+        
     
     print("FINISHIED!")
     core.quit()
