0ad116d0b49eef52a35b1fc9ca35593810630a68
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-04-18 10:14:20 +0800

    新增self-pacedR與正確率的畫圖指令

--- figure_plot.py
+++ figure_plot.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     result_data_path = "/Users/neuroling/Downloads/DINGHSIN_Results/2nd_Stim-results_selfPRT_PLDT/"
     
+    """
     # High Correctness group's RT
     y1_High_CD_RT_Mean_HighC = [783.1667, 879.3667]  #[783.1667, 879.3667, 1337.7, 1584.9333]  #[1100.9534, 958.6333, 783.1667, 1584.9333, 1337.7, 879.3667]
     y2_Low_CD_RT_Mean_HighC = [756.8841,869.1667]  #[756.8841,869.1667,1236.9333, 1417.8]   #[1118.6413, 845.6, 756.8841, 1417.8, 1236.9333, 869.1667]
@@ -42,4 +43,28 @@ if __name__ == "__main__":
     plt.xlabel("PLDT Correctness") # x label
     plt.legend()
     
-    plt.savefig(result_data_path + "LTTC_HCo-and-LCo_RT-Correctness_plt(without Pilots).jpg")
+    plt.savefig(result_data_path + "LTTC_HLCo_RT_plt(without Pilots).jpg")
+    """
+    
+    
+    # Self-Paced Reading Duration and Mean
+    
+    y1_Self_paced_DurMean_HighC = [77897.03333, 93289.36667]
+    y2_Self_paced_DurMean_LowC = [150670.2333, 111539.6333]
+    
+    
+    x1_High_Correctness = [76.67, 84.17]   #[55.83, 65.83, 76.67, 30, 62.5, 84.17]
+    x2_Low_Correctness = [30,62.5]
+    
+    
+    # High-C & High/Low-CD RT plot
+    plt.plot(x1_High_Correctness,y1_Self_paced_DurMean_HighC, label = "HC Self-paced Mean")
+    plt.plot(x2_Low_Correctness,y2_Self_paced_DurMean_LowC, label = "LC Self-paced Mean")
+    
+    
+    plt.title("High/Low Correctness v.s Self-paced Reading Duration Mean") # title
+    plt.ylabel("Reading Duration") # y label
+    plt.xlabel("PLDT Correctness") # x label
+    plt.legend()
+    
+    plt.savefig(result_data_path + "LTTC_HLCo_Self-pacedReading_plt(without Pilots).jpg")
