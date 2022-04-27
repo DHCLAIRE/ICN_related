52f4c8534706c8983a4060ffebfbebe792c24aef
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-04-27 09:03:57 +0800

    新增計算步驟的筆記和一些文本前處理步驟

--- Importance_Fractality.py
+++ Importance_Fractality.py
@@ -51,7 +51,70 @@ import json
 if __name__ == "__main__":
     
     """
+    
+    % directly from the paper>>
+    
+    Box counting is a practical method for computing the fractal dimension of words. 
+    To perform this calculation, the text should be divided into boxes of size s. 
+    It means that any box contains s successive words. 
+    The number of such boxes that contains at least one occurrence of a given word w
+    , i.e. the number of filled boxes, is N b ( w, s ) and the self-similarity property 
+    is expressed as, N b (w, s ) ∼s −D w . (1) where D w stands for the fractal dimension of word w.
+    It is obtained by finding the slope of log-log plot of N b ( w, s ) versus s for large enough s .
+    
+    
+    =======
     Number of shuffled box count(Nsh-b(s, w)) = M(The frequency of the word ) / 1+(M-1/N-1)(s-1)
     N = the number of word in a text
     s = the box size = how many words were count as one box
+    
+    =====
+    THE STEPS:
+    *For the values that based on SHUFFLED position*
+    1. Segment the words: Cut the word into different box sizes
+    2. Count the filled boxes: Count how many times of the target word that appeared in the divided boxes
+    3. GET THE VALUE OF THE COUNTS
+    
+    *For the values that based on SHUFFLED position*
+    1. Shuffle the orginal text: Shuffled the text by the unit of word(Randomly distributed)
+    2. Segment the words: Cut the word into different box sizes
+    3. Count the filled boxes: Count how many times of the target word that appeared in the divided boxes
+    
+    
+    Calculate the Fractality: 
+    
+    
+    
+    
     """
+    
+    """
+    String preprocessing (Things that need to do first)
+    1. converting all letters to lower or upper case
+    2. converting numbers into words or removing numbers
+    3. removing punctuations, accent marks and other diacritics
+    4. removing white spaces
+    5. expanding abbreviations
+    6. removing stop words, sparse terms, and particular words
+    7. text canonicalization
+    """
+    
+    """
+    What is the base of the calculation?? >> the whole chapter?  Or each sentence?? 
+    """
+    
+    tmpLIST = []
+    
+    
+    testing_text = """The marathon COVID-19 lockdown in Sydney, Australia, ended Monday for vaccinated residents. Stay-at-home orders imposed on June 26 have been lifted. Government advertisements have promised that freedoms would return when vaccination rates passed certain milestones. The message has been getting through to the community. Lockdown in the New South Wales state capital, Sydney, was lifted Monday because inoculation rates have passed 70% for people above aged 16. Shops have reopened for the first time since June. Small gatherings at home are permitted, and larger groups are allowed to meet at parks and beaches. However, the above apply only to fully vaccinated people. All residents still face restrictions on travel beyond Sydney. The rules will be eased when vaccination rates in New South Wales reach 80%. At that point international travel will resume. Still, New South Wales state premier Dominic Perrottet stated that a cautious stages approach to reopening is needed."""
+    print(type(testing_text))
+    print(testing_text)
+    
+    tmpLIST = testing_text.lower().split(" ")
+    
+    pprint(tmpLIST)
+    print(len(tmpLIST))
+    
+    for strROW in tmpLIST:
+        print(strROW)
+        #print(type(strROW))
