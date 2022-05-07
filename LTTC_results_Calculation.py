90338768bdbf170ed0fc499cbd41b1777e6c23d7
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-05-07 13:18:04 +0800

    刪除廢碼

--- LTTC_results_Calculation.py
+++ LTTC_results_Calculation.py
@@ -102,123 +102,47 @@ if __name__ == "__main__":
         print(len(resultLIST))
         
         # exclude the blank row
-        for row in resultLIST:
-            if len(row) == 0:
-                resultLIST.pop(resultLIST.index(row))
-            else:
-                pass
-        print(len(resultLIST))
+    for row in resultLIST:
+        if len(row) == 0:
+            resultLIST.pop(resultLIST.index(row))
+        else:
+            pass
+    print(len(resultLIST))
         
-        # finding the wanted H & L CD response
-        for row in resultLIST:
-            rawLIST = row.split(",")
-            if rawLIST[2] in High_CDpwLIST:
-                print("High CD pw: ", rawLIST)
-                H_CD_rawLIST.append(rawLIST)
-            elif rawLIST[2] in Low_CDpwLIST:
-                print("Low CD pw: ", rawLIST)
-                L_CD_rawLIST.append(rawLIST)
-            else:
-                pass
-        print(H_CD_rawLIST)
-        print(len(H_CD_rawLIST))
-        print(L_CD_rawLIST)
-        print(len(L_CD_rawLIST))
+    # finding the wanted H & L CD response
+    for row in resultLIST:
+        rawLIST = row.split(",")
+        if rawLIST[2] in High_CDpwLIST:
+            print("High CD pw: ", rawLIST)
+            H_CD_rawLIST.append(rawLIST)
+        elif rawLIST[2] in Low_CDpwLIST:
+            print("Low CD pw: ", rawLIST)
+            L_CD_rawLIST.append(rawLIST)
+        else:
+            pass
+    print(H_CD_rawLIST)
+    print(len(H_CD_rawLIST))
+    print(L_CD_rawLIST)
+    print(len(L_CD_rawLIST))
         
-        for row in H_CD_rawLIST:
-            H_rtLIST.append(float(row[5]))
-            if 0. in H_rtLIST:
-                H_rtLIST.remove(0.0)
-            else:
-                pass
-        print(H_rtLIST)
-        print(len(H_rtLIST))
-        print(sum(np.array(H_rtLIST)))
-        print(round(np.mean(np.array(H_rtLIST)),3))
-        
-        for row in L_CD_rawLIST:
-            L_rtLIST.append(float(row[5]))
-            if 0. in L_rtLIST:
-                L_rtLIST.remove(0.0)
-            else:
-                pass
-        print(L_rtLIST)
-        print(len(L_rtLIST))
-        print(sum(np.array(L_rtLIST)))
-        print(round(np.mean(np.array(L_rtLIST)),3))
-        
-        
-        """
-            # calculate PLDT_mean (H & L) per sub
-            rtLIST.append(float(rawLIST[5]))
-        print(rtLIST)
-        print(len(rtLIST))
-        
-        # exclude the N/A response
-        for rt in rtLIST:
-            if 0. in rtLIST:
-                rtLIST.remove(0.0)
-            else:
-                pass
-        print(rtLIST)
-        print(len(rtLIST))
-        
-        #print(np.array(rtLIST))
-        #print(len(np.array(rtLIST)))
-        #print(type(np.array(rtLIST)))
-        ##rtARRAY = np.array(rtLIST)
-        """
-        """
-        print("High-CD RT LIST = ", High_CDRT_LIST)
-        print(len(High_CDRT_LIST))
-        print("Low-CD RT LIST = ", Low_CDRT_LIST)
-        print(len(Low_CDRT_LIST))
-        
-        for HighRT_STR in High_CDRT_LIST:
-            if len(HighRT_STR) == 1:
-                High_CDRT_LIST.remove(HighRT_STR)
-                final_High_RT_LIST = High_CDRT_LIST
-            else:
-                final_High_RT_LIST = High_CDRT_LIST
-        print(final_High_RT_LIST)
-        print(len(final_High_RT_LIST))
-        
-        for LowRT_STR in Low_CDRT_LIST:
-            if len(LowRT_STR) == 1:
-                Low_CDRT_LIST.remove(LowRT_STR)
-                final_Low_RT_LIST = Low_CDRT_LIST
-            else:
-                final_Low_RT_LIST = Low_CDRT_LIST
-                
-        print(final_Low_RT_LIST)
-        print(len(final_Low_RT_LIST))
-        
-        High_Mean = RT_Mean(final_High_RT_LIST)  #round(Sum(final_High_RT_LIST)/len(final_High_RT_LIST), 4)
-        Low_Mean = RT_Mean(Low_CDRT_LIST)
-        
-        print(High_Mean)
-        print(type(High_Mean))
-        print(Low_Mean)
-        print(type(Low_Mean))
-        
-        RT_LIST = ['1481', '948', '821', '1186', ' ', '0']
-        
-        
-        for STR in RT_LIST:
-            if len(STR) == 1:
-                if STR == '0' or STR != '0':
-                    print("Ya")
-                    RT_LIST.remove(STR)
-                    final_RT_LIST = RT_LIST
-                else:
-                    pass
-            else:
-                final_RT_LIST = RT_LIST
-                
-        print(final_RT_LIST)
-        print(len(final_RT_LIST))
-        
-        
-    # For SelfPRT_Mean
-    #with
-    """
+    for row in H_CD_rawLIST:
+        H_rtLIST.append(float(row[5]))
+        if 0. in H_rtLIST:
+            H_rtLIST.remove(0.0)
+        else:
+            pass
+    #print(H_rtLIST)
+    print(len(H_rtLIST))
+    H_PLDTmean_subFLOAT = round(np.mean(np.array(H_rtLIST)),3)
+    print("H pwRT Mean :", H_PLDTmean_subFLOAT)
+    
+    for row in L_CD_rawLIST:
+        L_rtLIST.append(float(row[5]))
+        if 0. in L_rtLIST:
+            L_rtLIST.remove(0.0)
+        else:
+            pass
+    #print(L_rtLIST)
+    print(len(L_rtLIST))
+    L_PLDTmean_subFLOAT = round(np.mean(np.array(L_rtLIST)),3)
+    print("L pwRT Mean :", L_PLDTmean_subFLOAT)
