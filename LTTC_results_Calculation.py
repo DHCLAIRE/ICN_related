0d2ae4a48e48ce5a9937b3195d7581d6c5af1ddb
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-05-15 20:54:00 +0800

    (未成功)想弄成大Loop跑正確率和文本評分，結果完全弄亂了

--- LTTC_results_Calculation.py
+++ LTTC_results_Calculation.py
@@ -85,7 +85,7 @@ def correctness(resultLIST, typeSTR = None):
 
 if __name__ == "__main__":
     result_data_path = "/Users/neuroling/Downloads/DINGHSIN_Results/2nd_Stim-results_selfPRT_PLDT/"
-    """
+    
     for z in range(3):
         # Setting up the data_path
         result_data_path = "/Users/neuroling/Downloads/DINGHSIN_Results/2nd_Stim-results_selfPRT_PLDT/"
@@ -191,8 +191,8 @@ if __name__ == "__main__":
         print("{}:".format(sub_num), H_pwRT_DICT["High-CD RT Mean"], L_pwRT_DICT["Low-CD RT Mean"],PLDT_correct_subDICT["PLDT-total Correctness"],H_PLDT_correct_subDICT["H-CD PLDT Correctness"],L_PLDT_correct_subDICT["L-CD PLDT Correctness"])
         #print(H_pwRT_DICT["High-CD RT Mean"], L_pwRT_DICT["Low-CD RT Mean"],PLDT_correct_subDICT["PLDT-total Correctness"],H_PLDT_correct_subDICT["H-CD PLDT Correctness"],L_PLDT_correct_subDICT["L-CD PLDT Correctness"])
     
-    """
     
+        """
     resultLIST = []
     tmpLIST = []
 
@@ -216,131 +216,130 @@ if __name__ == "__main__":
         print(sub_num, "Target pw : ", targetPseudoLIST)
         print(sub_num, "High-CD pw : ", High_CDpwLIST)
         print(sub_num, "Low-CD pw : ", Low_CDpwLIST) # output: 003 Low-CD pw :  ['vaesow', 'payliy', 'paenliy']
+        """
     
     
+        # Self_rating section
+        readingLIST = []
+        ratingDICT = {}
+        count = 0
+        cleaned_LIST = []
+        textLIST = []
     
-    # Self_rating section
-    readingLIST = []
-    ratingDICT = {}
-    count = 0
-    cleaned_LIST = []
-    textLIST = []
-    
-    # Load in the self-rating score
-    with open (result_data_path + "003_Reading_task.csv", "r", encoding= 'unicode_escape') as csvfile_reading:  #, "r", encoding = "utf-8")
-        readingLIST = csvfile_reading.read().split("\n")
-        #pprint(readingLIST)
-        #print(type(readingLIST))
-        print(len(readingLIST))
-        readingLIST.pop(0)   # exclude the headers
-        print(len(readingLIST))
+        # Load in the self-rating score
+        with open (result_data_path + "{}_Reading_task.csv".format(sub_num), "r", encoding= 'unicode_escape') as csvfile_reading:  #, "r", encoding = "utf-8")
+            readingLIST = csvfile_reading.read().split("\n")
+            #pprint(readingLIST)
+            #print(type(readingLIST))
+            #print(len(readingLIST))
+            readingLIST.pop(0)    # exclude the headers
+            #print(len(readingLIST))
         
-        # exclude the blank row
-        readingLIST = LISTblankEraser(readingLIST)
-        print(len(readingLIST))
+            # exclude the blank row
+            readingLIST = LISTblankEraser(readingLIST)
+            #print(len(readingLIST))
         
-        # Divided the item in str type into 2 part for finding out the text
-        for row in readingLIST:
-            # Check the puntuation to split the text
-            if ',"[""' in row:
-                rawLIST = row.split(',"[""')
-            elif ',"[\'' in row:
-                rawLIST = row.split(',"[\'')
-            # if there's new type of punctuation, the script would let you know the str item hasn't been split
-            else:
-                print("Wrong!!!!!!!!!!!!!!!!!! >>>>>",rawLIST)
-                print("Wrong_count >>>>>", len(rawLIST))
-            
-            # Check if the rawLIST been properly divided
-            if len(rawLIST) ==2:
-                count +=1
-            else:
-                print("Wrong!!!!!!!!!!!!!!!!!! >>>>>",rawLIST)
-                print("Wrong_count >>>>>", len(rawLIST))
-            cleaned_LIST.append(rawLIST)
-
-        print(count)
-        #pprint(cleaned_LIST)
-        print(len(cleaned_LIST))
-        
-        # to check the PWs that are in each text
-        for row in cleaned_LIST:
-            # lowercase all the words, split them into each word per unit
-            textLIST = row[1].lower().split(" ")
-            print(textLIST)
-            print(len(textLIST))
-            
-            # print out the PWs according to the squence of the text and their CD condition
-            for word in textLIST:
-                #print(word)
-                if word in High_CDpwLIST:
-                    print(cleaned_LIST.index(row), "High-CD", word)
-                if word in Low_CDpwLIST:
-                    print(cleaned_LIST.index(row), "Low-CD", word)
+            # Divided the item in str type into 2 part for finding out the text
+            for row in readingLIST:
+                # Check the puntuation to split the text
+                if ',"[""' in row:
+                    rawLIST = row.split(',"[""')
+                elif ',"[\'' in row:
+                    rawLIST = row.split(',"[\'')
+                    # if there's new type of punctuation, the script would let you know the str item hasn't been split
                 else:
-                    pass
-                
-        # set out the count and blank LIST for further use
-        count_H = 0
-        count_L = 0
-        H_textLIST = []
-        L_textLIST = []
-        new_H_textLIST = []
-        new_L_textLIST = []
-        H_ratingDICT = {}
-        L_ratingDICT = {}
-        
-        # collect the H & L CD text based on the set PWs
-        for row in cleaned_LIST:
-            textLIST = row[1].lower().split(" ")
+                    print("Wrong!!!!!!!!!!!!!!!!!! >>>>>",rawLIST)
+                    print("Wrong_count >>>>>", len(rawLIST))
             
-            # to go through each word inside each text for finding whether the pw is in the H- or L-CD_pwLIST, and then put those info into LIST by their CD types
-            for word in textLIST:
-                # if the pw is one of the H-CD pw, then we would put this trial as the H_textLIST, also ignor other words that inside the same text
-                for H_pw in High_CDpwLIST:
-                    if H_pw in word:
-                        print("High-CD", word)
-                        count_H += 1
-                        H_textLIST.append(row)
-                    else:
-                        pass
-                    
-                # if the pw is one of the H-CD pw, then we would put this trial as the H_textLIST, also ignor other words that inside the same text
-                for L_pw in Low_CDpwLIST:
-                    if L_pw in word:
-                        print("Low-CD", word)
-                        count_L += 1
-                        L_textLIST.append(row)
+                # Check if the rawLIST been properly divided
+                if len(rawLIST) ==2:
+                    count +=1
+                else:
+                    print("Wrong!!!!!!!!!!!!!!!!!! >>>>>",rawLIST)
+                    print("Wrong_count >>>>>", len(rawLIST))
+                cleaned_LIST.append(rawLIST)
+            
+            print(count)
+            #pprint(cleaned_LIST)
+            print(len(cleaned_LIST))
+        
+            # to check the PWs that are in each text
+            for row in cleaned_LIST:
+                # lowercase all the words, split them into each word per unit
+                textLIST = row[1].lower().split(" ")
+                #print(textLIST)
+                #print(len(textLIST))
+            
+                # print out the PWs according to the squence of the text and their CD condition
+                for word in textLIST:
+                    #print(word)
+                    if word in High_CDpwLIST:
+                        print(cleaned_LIST.index(row), "High-CD", word)
+                    if word in Low_CDpwLIST:
+                        print(cleaned_LIST.index(row), "Low-CD", word)
                     else:
                         pass
                 
-        #print("count_H", count_H)
-        #print("count_L", count_L)
-        #print("Text_Hs", H_textLIST)
-        #print(len(H_textLIST))
-        #print("Text_Ls", L_textLIST)
-        #print(len(L_textLIST))
-        ##print(len(readingLIST[0]))
+            # set out the count and blank LIST for further use
+            count_H = 0
+            count_L = 0
+            H_textLIST = []
+            L_textLIST = []
+            new_H_textLIST = []
+            new_L_textLIST = []
+            H_ratingDICT = {}
+            L_ratingDICT = {}
         
-        # to accumulate the rating score according to the CD types
-        for row in H_textLIST:
-            rawLIST = row[0].split(",")
-            rawLIST.append(row[1])
-            new_H_textLIST.append(rawLIST)
-        #print(new_H_textLIST)
-        #print(len(new_H_textLIST))
+            # collect the H & L CD text based on the set PWs
+            for row in cleaned_LIST:
+                textLIST = row[1].lower().split(" ")
+            
+                # to go through each word inside each text for finding whether the pw is in the H- or L-CD_pwLIST, and then put those info into LIST by their CD types
+                for word in textLIST:
+                    # if the pw is one of the H-CD pw, then we would put this trial as the H_textLIST, also ignor other words that inside the same text
+                    for H_pw in High_CDpwLIST:
+                        if H_pw in word:
+                            #print("High-CD", word)
+                            count_H += 1
+                            H_textLIST.append(row)
+                        else:
+                            pass
+                
+                    # if the pw is one of the H-CD pw, then we would put this trial as the H_textLIST, also ignor other words that inside the same text
+                    for L_pw in Low_CDpwLIST:
+                        if L_pw in word:
+                            #print("Low-CD", word)
+                            count_L += 1
+                            L_textLIST.append(row)
+                        else:
+                            pass
+                
+            #print("count_H", count_H)
+            #print("count_L", count_L)
+            #print("Text_Hs", H_textLIST)
+            #print(len(H_textLIST))
+            #print("Text_Ls", L_textLIST)
+            #print(len(L_textLIST))
+            ##print(len(readingLIST[0]))
         
-        for row in L_textLIST:
-            rawLIST = row[0].split(",")
-            rawLIST.append(row[1])
-            new_L_textLIST.append(rawLIST)
-        #print(new_L_textLIST)
-        #print(len(new_L_textLIST))
+            # to accumulate the rating score according to the CD types
+            for row in H_textLIST:
+                rawLIST = row[0].split(",")
+                rawLIST.append(row[1])
+                new_H_textLIST.append(rawLIST)
+                #print(new_H_textLIST)
+                #print(len(new_H_textLIST))
         
-        # calculate the self-rating mean of H & L CD texts
-        H_ratingDICT = Mean(new_H_textLIST, 4, "H-CD self-rating")
-        L_ratingDICT = Mean(new_L_textLIST, 4, "L-CD self-rating")
+            for row in L_textLIST:
+                rawLIST = row[0].split(",")
+                rawLIST.append(row[1])
+                new_L_textLIST.append(rawLIST)
+            #print(new_L_textLIST)
+            #print(len(new_L_textLIST))
         
-        print(H_ratingDICT)
-        print(L_ratingDICT)
-    
+            # calculate the self-rating mean of H & L CD texts
+            H_ratingDICT = Mean(new_H_textLIST, 4, "H-CD self-rating")
+            L_ratingDICT = Mean(new_L_textLIST, 4, "L-CD self-rating")
+        
+            print(H_ratingDICT)
+            print(L_ratingDICT)
