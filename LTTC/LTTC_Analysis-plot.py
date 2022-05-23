a160cd6a77c2a4698c94f44a419b47caa0644549
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-05-23 16:08:47 +0800

    成功plot出H_RT & 正確率的scatter圖

--- LTTC/LTTC_Analysis-plot.py
+++ LTTC/LTTC_Analysis-plot.py
@@ -32,21 +32,91 @@ if __name__ == "__main__":
     result_data_path = "/Users/neuroling/Downloads/DINGHSIN_Results/2nd_Stim-results_selfPRT_PLDT/"
     
     resultLIST = []
-    x_LIST = []
-    y_LIST = []
+    n_resultLIST = []
+    
+    # RT
+    H_rawRT_LIST = []
+    L_rawRT_LIST = []
+    H_finalRT_LIST = []
+    L_finalRT_LIST = []
+    
+    # Correctness
+    ALL_Correctness_LIST = []
+    H_raw_Correctness_LIST = []
+    L_raw_Correctness_LIST = []
+    H_final_Correctness_LIST = []
+    L_final_Correctness_LIST = []
+    
+    # Self-rating 
+    Self_rating_Mean_LIST = []
+    H_Self_rating_Mean_LIST = []
+    L_Self_rating_Mean_LIST = []
+    
+    # Self-reading
+    Self_readingT_minMean_LIST = []
+    H_Self_readingT_minMean_LIST = []
+    L_Self_readingT_minMean_LIST = []
     
     
     with open (result_data_path + "PLDT_analyzed_results.csv", "r", encoding = "utf-8") as csvfile:
+        """
         resultLIST = csvfile.read().split("\n")
         resultLIST.pop(0)
         resultLIST = LISTblankEraser(resultLIST)
+        print(len(resultLIST))
+        """
         
-        for row in resultLIST:
+        data = pd.read_csv(csvfile, index_col=0)
+        data.plot.scatter(x = "(%)ALL_Correctness", y = "(ms)H_raw_RTMean", alpha = 0.5)
+        data.plot()
+        
+    """
+    for row in resultLIST:
+        rawLIST = row.split(",")
+        rawLIST = np.array(rawLIST)
+        n_resultLIST.append(rawLIST)
+        
+    print(n_resultLIST)
+    print(type(n_resultLIST))
+        # Setting the wanted X & Y axis
+    
+    data = pd.DataFrame(np.array(n_resultLIST), columns=['Sub_id',
+                                                       '(ms)H_raw_RTMean',
+                                                       '(ms)H_final_RTMean',
+                                                       '(ms)L_raw_RTMean',
+                                                       '(ALL_Correctnessms)L_final_RTMean',
+                                                       '(%)ALL_Correctness',
+                                                       '(%)H_raw_Correctness',
+                                                       '(%)H_final_Correctness',
+                                                       '(%)L_raw_Correctness',
+                                                       '(%)L_final_Correctness',
+                                                       'Self_rating_Mean',
+                                                       'H_Self_rating_Mean',
+                                                       'L_Self_rating_Mean',
+                                                       '(ms)Self_readingT_msMean',
+                                                       '(ms)H_Self_readingT_msMean',
+                                                       '(ms)L_Self_readingT_msMean',
+                                                       '(min)Self_readingT_minMean',
+                                                       '(min)H_Self_readingT_minMean',
+                                                       '(min)L_Self_readingT_minMean'])
+    """
+
+    #plt.plot(data)
+    #plot(x = '(%)ALL_Correctness', y = )
+    
+    """
+    for row in resultLIST:
             rawLIST = row.split(",")
             
-            x_LIST.append(rawLIST[1])
-            y_LIST.append(rawLIST[5])
-        print(x_LIST)
-        print(y_LIST)
+            ALL_Correctness_LIST.append(rawLIST[5])   # correctness
+            H_rawRT_LIST.append(rawLIST[1])   # RT(High & Low)
+            L_rawRT_LIST.append(rawLIST[3])
         
         
+        # Plotting section
+        
+        # ploting the RT & Correctness
+        plt_1.scatter(ALL_Correctness_LIST, H_rawRT_LIST)
+        plt_2.scatter(ALL_Correctness_LIST, L_rawRT_LIST)
+        plt.show()
+        """ 
