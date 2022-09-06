3f9209c8d7acf2155262c27692cffc76b045c299
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-09-06 14:19:57 +0800

    (測試成功)新增MEG Trigger及相關指令

--- LTTC/LDT_Win_MEGver.py
+++ LTTC/LDT_Win_MEGver.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import psychopy
-from psychopy import visual, core, event, clock
+from psychopy import visual, core, event, clock, parallel
 import json
 import random
 from random import sample
@@ -26,7 +26,7 @@ def display_ins(STR, keyPressLIST = None):
     '''
     instructionsLIST = STR.split("\\\\")
     keyPressLIST = keyPressLIST
-        
+
     for t in instructionsLIST:
         instructions = visual.TextStim(win = win, text = t)
         instructions.draw()
@@ -42,21 +42,14 @@ def display_fix():
     fixation.draw()
     win.flip()
 
-# Trigger port info for MEG
-p = parallel.ParallelPort('0x0378')
+# The MEG trigger port info
+port = parallel.ParallelPort('0x0378')
 
 if __name__ == "__main__":
-    
-    # Step_0: load in all the stimuli
-    # testing stimuli (realwordLIST & pseudowordLIST)
-    #realwordLIST = ["blue", "green", "yellow", "red", "orange"]
-    #pseudowordLIST = ["thorpt", "rairn", "coan", "flatch", "meeg"]
-    
-    
-    #sub_id = str(input("Subject: "))
-    stim_data_path = "C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-Materials/"
-    result_data_path = "C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-results_selfP_PLDT/"
-    
+
+    stim_data_path = "I:/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_material_2nd/2nd_Stim-Materials/"
+    result_data_path = "I:/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_material_2nd/2nd_Stim-results_selfPRT_PLDT/"
+
     # setting up usable dataLIST
     pseudoLIST = []
     targetPseudoLIST = []
@@ -69,24 +62,24 @@ if __name__ == "__main__":
     with open (stim_data_path + "LTTC-pseudowordLIST.json", "r", encoding = "utf-8") as jfile:
         pseudoLIST = json.load(jfile)
         #print("12 pseudowords = ", pseudoLIST)
-        
+
     # 3_Randomly select 6 out of the list of 12 pseudowords as the target words
         # randomly select 6 target pseudowords from the list
         targetPseudoLIST = sample(pseudoLIST, 6)
-        
+
         # collect other 6 pseudowords as the control group
         for t in pseudoLIST:
             if t not in targetPseudoLIST:
                 controlPseudoLIST.extend([t])
             else:
                 pass
-            
+
         #print("The TargetPseudo words = ", targetPseudoLIST)
         #print("The ControlPseudo words = ", controlPseudoLIST)
-        
+
     # 4_Select 3 out of the 6 target words and divided 3-3 into High-CD and Low-CD conditions
         words_high_CD_setLIST = sample(targetPseudoLIST, 3)
-        
+
         for w in targetPseudoLIST:
             if w not in words_high_CD_setLIST:
                 words_low_CD_setLIST.extend([w])
@@ -98,30 +91,30 @@ if __name__ == "__main__":
 
         random.shuffle(pseudoLIST)
         """
-    
+
     # key in number for notifying which subject it is
     sub_id = str(input("Subject: "))
-    
+
     #剩把pseudoDICT的值叫出來
     pseudoLIST = []
     targetPseudoLIST = []
     controlPseudoLIST = []
     words_high_CD_setLIST = []
     words_low_CD_setLIST = []
-    
-    
+
+
     DICT_name = sub_id + '_pseudowordsDICT.json'
     Dsave_path = result_data_path + DICT_name
-    
+
     with open (Dsave_path, "r", encoding = "utf-8") as jfile:
         pseudoDICT = json.load(jfile)
         pprint(pseudoDICT)
         #print(pseudoDICT["High_CD condition pseudowords_3"])
         pseudoLIST.extend(pseudoDICT["The ControlPseudo group_6"])
         pseudoLIST.extend(pseudoDICT["The TargetPseudo group_6"])
-        
+
         targetPseudoLIST.extend(pseudoDICT["The TargetPseudo group_6"])
-        
+
     pass
 
 
@@ -137,14 +130,14 @@ if __name__ == "__main__":
     correctnessLIST = []
     responseLIST = []
     correctLIST = []
-    
+
     # key in number for notifying which subject it is
     #sub_id = str(input("Subject: "))
-    
+
     # Step_1: Show the instructions
     # Setting the presented window
-    #win = visual.Window(size = [500, 500],color = [-1, -1, -1], units ="pix")
-    win = visual.Window(color = [-1, -1, -1], units ="pix", fullscr = True)
+    win = visual.Window(size = [500, 500],color = [-1, -1, -1], units ="pix")
+    #win = visual.Window(color = [-1, -1, -1], units ="pix", fullscr = True)
     clock = core.Clock()
     #start_time = clock.getTime()  >>change position to make the calculation correct
 
@@ -152,37 +145,43 @@ if __name__ == "__main__":
     instructions_1 = """接下來你會看到一連串的字詞，\n請依照實驗指示進行按鍵反應，\n當你準備好的時候，\n請按下空白鍵"""
     instructions_2 = """看過請按z 沒看過請按/\n請按空白鍵繼續\\\\將你的左食指輕放在z鍵，右食指輕放在/鍵。\n請按空白鍵繼續\\\\當字詞出現時，請盡快且正確的進行按鍵反應。\n請按空白鍵繼續"""
     keypress = ['space']
-    
+
     core.wait(3)
-    
+
     #Display the instructions
     display_ins(instructions_1, keypress)
     display_ins(instructions_2, keypress)
-    
+
     core.wait(3)
-    
+
     # 假詞all重新排列後依序送出，整個LIST重複送10次
     # Step_4: show the stimuli(real words or pseudowords), and remain the stimuli for 400ms  # randomly display would also be crucial!!
-    for i in range(10):
-        
-        # randomly select the wanted pseudoword from the list  
+    for i in range(1):
+
+        # randomly select the wanted pseudoword from the list
         random.shuffle(pseudoLIST)
         for stim_wordSTR in pseudoLIST:
             # display fixation in the central of the screen
             display_fix()
             core.wait(1)
-            
+
             start_time = clock.getTime()
             # Show the testing stimulus
             testing_stimuli = visual.TextStim(win = win, text = stim_wordSTR)  # how to control that every words only
             testing_stimuli.draw()
+
+            # TO MARK THE PSEUDOWORD APPEARED
+            port.setData(8) #This is open the trigger
+            #core.wait(0.01) # Stay for 10 ms
+            #port.setData(0) #This is close the trigger
+
             win.flip()  # always add this after an item was presented
-        
+
             #setting up what keypress would allow the experiment to proceed
             keys = event.waitKeys(maxWait = 5, keyList = ['z', 'slash'])
             event.getKeys(keyList = ['z', 'slash'])
             print(keys)
-        
+
             # 再加上if else的判斷決定是否要收或是要怎麼紀錄這反應
             if keys == ["z"]:
                 conditionLIST = ["seen"]
@@ -191,7 +190,7 @@ if __name__ == "__main__":
                 print(time_duration)
                 #print(type(time_duration))
                 clock.reset()
-        
+
             elif keys == ["slash"]:
                 conditionLIST = ["unseen"]
                 end_time = clock.getTime()
@@ -199,14 +198,18 @@ if __name__ == "__main__":
                 print(time_duration)
                 #print(type(time_duration))
                 clock.reset()
-            
+
             else:
                 keys = ["Wrong!!"]
                 conditionLIST = ["N/A"]
                 time_duration = 0
                 print(time_duration)
                 clock.reset()
-        
+            # TO MARK THE PSEUDOWORD GONE
+            #port.setData(8) #This is open the trigger
+            #core.wait(0.01) # Stay for 10 ms
+            port.setData(0) #This is close the trigger
+
             # calculate the correctness of the LDT response
             if stim_wordSTR in targetPseudoLIST:
                 #conditionLIST = ["seen"]
@@ -217,7 +220,7 @@ if __name__ == "__main__":
                     correctLIST = ["False"]
                 else:
                     correctLIST = ["N/A"]
-        
+
             elif stim_wordSTR not in targetPseudoLIST:
                 #conditionLIST = ["seen"]
                 if keys == ["z"]:
@@ -229,8 +232,8 @@ if __name__ == "__main__":
                     correctLIST = ["N/A"]
             else:
                 pass
-            
-        
+
+
             # making the wanted info into the List form for future use
             sub_idLIST.append(sub_id)
             dateLIST.append(day)
@@ -239,13 +242,13 @@ if __name__ == "__main__":
             responseLIST.append(conditionLIST)
             LDT_rtLIST.append(time_duration)
             correctnessLIST.append(correctLIST)
-        
+
             #core.wait(0.5)
-        
+
             # close the window  at the end of the experiment
     win.close()
-        
-        
+
+
     # Saving the self_paced_rt result into csv file
     dataDICT = pd.DataFrame({'Sub_id':sub_idLIST,
                            'Date':dateLIST,
@@ -255,94 +258,11 @@ if __name__ == "__main__":
                            'LDT_RT':LDT_rtLIST,
                            'Correctness':correctnessLIST
                            })
-    
+
     #data_path = "/Users/ting-hsin/Docs/Github/ICN_related/"
     file_name = sub_id + '_LDT_results.csv'
     save_path = result_data_path + file_name
     dataDICT.to_csv(save_path, sep = "," ,index = False , header = True, encoding = "UTF-8")
-    
+
     # close all the possible ongoing commands that could be running in the background
     core.quit()  # normally we would add it, in case that anything happen
-
-    """
-       # turn all info into dataframe, and then save it as a csv file  # >> rewrite the following info
-    data=pd.DataFrame({'sid':sub_id,
-                   'trial_list':trial,
-                   'duration':interval,
-                   'trial_no':trial_no,
-                    'date':date,
-                    'trigger':trial_trigger,
-                    'response_key':response,
-                    'rt':response_time
-                    })
-    
-    # close all the possible ongoing commands that could be running in the background
-    core.quit()  # normally we would add it, in case that anything happen
-    
-    # Below is the testing zone for testing RatingScale function.
-    win = visual.Window(size = [500, 500],color = [-1, -1, -1], units ="pix")
-    
-    ratingScale = visual.RatingScale(win)
-    item = visual.TextStim(win = win, text = "THIS IS A TESTING SENTENCE!")
-    
-    
-    while ratingScale.noResponse:
-        item.draw()
-        ratingScale.draw()# the rating scale could work,but don't know why it can not be clicked
-        keys = event.waitKeys(maxWait = 10, keyList = ['z', 'slash'])
-        event.getKeys(keyList = ['z', 'slash'])
-        
-        
-        if keys == ["z"]:
-            keys = ["real_word"]
-            #end_time = clock.getTime()
-            #time_duration = round(end_time - start_time, 4)*1000    # normally 以毫秒作為單位
-            #print(time_duration)
-            #clock.reset()
-        
-        elif keys == ["slash"]:
-            keys = ["pseudoword"]
-            #end_time = clock.getTime()
-            #time_duration = round(end_time - start_time, 4)*1000    # normally 以毫秒作為單位
-            #print(time_duration)
-            #clock.reset()
-            
-        else:
-            keys = ["Wrong!!"]
-            #time_duration = 0
-            #print(time_duration)
-            #clock.reset()
-        
-
-        resultKeyLIST.append(keys)
-        
-        
-        
-        
-        
-        win.flip()
-    print(resultKeyLIST)
-    
-    rating = ratingScale.getRating()
-    decisionTime = ratingScale.getRT()
-    choiceHistory = ratingScale.getHistory()
-    
-    """
-
-    """
-    # NOTES:
-    
-    from psychopy.visual  # 主要用這個
-    
-    # 前面要先設定螢幕視窗
-    # win = psychopy.visual.Window(size = [500, 500], units ="pix")
-    
-    # key = psychopy.event.waitKeys()  # 按任何按鍵都會跳到下一張
-    # event.getKeys(keyList = ['space'])  # 指定按鍵才可以跳到下一張  
-    # 若指定按鍵沒有收到作業，這裡同時紀錄時間的要歸零，不然system會卡住
-    
-    #需要在寫if else 決定她按鍵反應時要怎記錄時間，然後press到哪個才會做反應
-    
-    # 按鍵反應 + 按鍵時間（RT）+ (trigger_for 腦波) + 正確與否
-    # basic info = trails數（的第幾題）,trai_list(第幾次trial), sub_num, date, duration
-    """
--- LTTC/LTTC_Reading_Win_MEGver.py
+++ LTTC/LTTC_Reading_Win_MEGver.py
@@ -4,7 +4,7 @@
 '''
 For stimuli formation and selection
 
-Steps: 
+Steps:
 1. (DONE)create a list that only have 12 pseudowords
 2. (DONE)import the pseudoword list
 3. (DONE)randomly select 6 out of the list of 12 pseudowords as the target words
@@ -26,7 +26,7 @@ from random import sample
 
 # for LDT and Reading Comprehension Task
 import psychopy
-from psychopy import visual, core, event, clock   #from psychopy import visual, core, event, gui, prefs, sound, monitors, clock,parallel
+from psychopy import visual, core, event, clock, parallel   #from psychopy import visual, core, event, gui, prefs, sound, monitors, clock,parallel
 #import json
 #import random
 import numpy as np
@@ -43,7 +43,7 @@ def display_ins(STR, keyPressLIST = None):
     '''
     instructionsLIST = STR.split("\\\\")
     keyPressLIST = keyPressLIST
-        
+
     for t in instructionsLIST:
         instructions = visual.TextStim(win = win, text = t)
         instructions.draw()
@@ -59,20 +59,20 @@ def display_fix():
     fixation.draw()
     win.flip()
 
-# Trigger port info for MEG
-p = parallel.ParallelPort('0x0378')
+# The MEG trigger port info
+port = parallel.ParallelPort('0x0378')
 
 if __name__ == "__main__":
-    stim_data_path = "C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-Materials/"
-    result_data_path = "C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-results_selfP_PLDT/"
-    text_data_path = "C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-Materials/USE_Output/LTTC_modifiedTexts_output/"
-    textSets_data_path = "C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-Materials/USE_Output/LTTC_modifiedTexts_output/LTTC_TextSets/"
+    stim_data_path = "I:/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_material_2nd/2nd_Stim-Materials/"
+    result_data_path = "I:/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_material_2nd/2nd_Stim-results_selfPRT_PLDT/"
+    #text_data_path = "C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-Materials/USE_Output/LTTC_modifiedTexts_output/"
+    textSets_data_path = "I:/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_material_2nd/2nd_Stim-Materials/USE_Output/LTTC_modifiedTexts_output/LTTC_TextSets/"
     #C:/Users/user/Documents/DINGHSIN/2020_LTTC/Experiment_materials/2nd_Stim-Materials/USE_Output/LTTC_modifiedTexts_output/LTTC_TextSets
-    
+
     # Stimuli Section
     # insert the stim-forming code?? >> or just present the selected texts per paragraphs??
-    
-    
+
+
     # Wanted data
     day = date.today()
     dateLIST = []
@@ -80,13 +80,13 @@ if __name__ == "__main__":
     self_paced_rtLIST=[]
     text_noLIST = []
     resultKeyLIST = []
-    
+
     # For key-in the id of the subject
     sub_id = str(input("Subject: "))
-    
+
     # setting up the display win conditions
-    #win = visual.Window(size = [500, 500],color = [-1, -1, -1], units ="pix")
-    win = visual.Window(color = [-1, -1, -1], units ="pix", fullscr = True)
+    win = visual.Window(size = [500, 500],color = [-1, -1, -1], units ="pix")
+    #win = visual.Window(color = [-1, -1, -1], units ="pix", fullscr = True)
     clock = core.Clock()
     #start_time = clock.getTime()
 
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     instructions_1 = """接下來你會看到一篇文章，\n請依照實驗指示進行按鍵反應，\n當你準備好的時候，\n請按下空白鍵"""
     instructions_2 = """請問對於剛剛那一篇文章理解了多少？\n請在紙上評分，評分完畢後\n請按下空白鍵繼續"""
     keypress = ['space']
-    
+
     # for pseudoword data
     tmpLIST = []
     tmpLIST_2 = []
@@ -105,30 +105,30 @@ if __name__ == "__main__":
     words_low_CD_setLIST = []
     texts_high_CD_setLIST = []
     texts_low_CD_setLIST = []
-    
-    
+
+
     # 2_Import the pseudoword list (in json file form)
     with open (stim_data_path + "LTTC-pseudowordLIST.json", "r", encoding = "utf-8") as jfile:
         pseudoLIST = json.load(jfile)
         print("12 pseudowords = ", pseudoLIST)
-        
+
     # 3_Randomly select 6 out of the list of 12 pseudowords as the target words
         # randomly select 6 target pseudowords from the list
         targetPseudoLIST = sample(pseudoLIST, 6)
-        
+
         # collect other 6 pseudowords as the control group
         for t in pseudoLIST:
             if t not in targetPseudoLIST:
                 controlPseudoLIST.extend([t])
             else:
                 pass
-            
+
         print("The TargetPseudo words = ", targetPseudoLIST)
         print("The ControlPseudo words = ", controlPseudoLIST)
-        
+
     # 4_Select 3 out of the 6 target words and divided 3-3 into High-CD and Low-CD conditions
         words_high_CD_setLIST = sample(targetPseudoLIST, 3)
-        
+
         for w in targetPseudoLIST:
             if w not in words_high_CD_setLIST:
                 words_low_CD_setLIST.extend([w])
@@ -137,8 +137,8 @@ if __name__ == "__main__":
 
         print("High-CD_set = ", words_high_CD_setLIST)
         print("Low-CD_set = ", words_low_CD_setLIST)
-        
-    
+
+
     # Load in the stim_texts
     # for the stim_texts data
     textSetsLIST_High = []
@@ -152,13 +152,13 @@ if __name__ == "__main__":
     total_stimSetLIST = []
     shuffledTotalT_LIST = []
     rating_LIST = []
-    
-    
+
+
     # for calling out the sets individually
     HightSetsLIST = []
     LowtSetsLIST = []
     Setsinfo_LIST = []
-    
+
     # High_CD Set TEXTS
     # texts_high_CD_setLIST = [345, 456, 567, 367, 347]
     HighCD_CallingLIST = [["3", "4", "5"], ["4", "5", "6"], ["5", "6", "7"], ["3", "6", "7"], ["3", "4", "7"]]
@@ -169,16 +169,16 @@ if __name__ == "__main__":
     print(HightSetsLIST[0][0])
     print(LowtSetsLIST)
     print(LowtSetsLIST[0][0])
-    
+
     Setsinfo_LIST.extend(HightSetsLIST)
-    Setsinfo_LIST.extend(LowtSetsLIST)    
-    
-    
+    Setsinfo_LIST.extend(LowtSetsLIST)
+
+
     for sets in range(3):
         # High_CD Set TEXTS
         with open (textSets_data_path + "sets_{}_LIST.json".format(HightSetsLIST[0][sets]), "r", encoding = "utf-8") as jfile_3:
             textSetsLIST_High = json.load(jfile_3)
-            
+
             # randomly select 5 texts from the json file
             High_stimLIST = random.sample(textSetsLIST_High, 5)
             #pprint(High_stimLIST)
@@ -189,58 +189,64 @@ if __name__ == "__main__":
                 new_tSTR = tSTR.replace("{}", words_high_CD_setLIST[sets])
                 #pprint(new_tSTR)
                 new_High_textSetsLIST.extend([new_tSTR])
-                
+
         # Low_CD Set TEXTS
         # texts_low_CD_setLIST = [128, 289, 890, 190, 120]
         with open (textSets_data_path + "sets_{}_LIST.json".format(LowtSetsLIST[0][sets]), "r", encoding = "utf-8") as jfile_4:
             textSetsLIST_Low = json.load(jfile_4)
-                    
+
             # randomly select 5 texts from the json file
             Low_stimLIST = random.sample(textSetsLIST_Low, 5)
             #pprint(Low_stimLIST)
             #print(len(Low_stimLIST))
-        
+
             # replace {} to the assigned pseudowords by different condition
             for tSTR in Low_stimLIST:
                 new_tSTR = tSTR.replace("{}", words_low_CD_setLIST[sets])
                 #pprint(new_tSTR)
                 new_Low_textSetsLIST.extend([new_tSTR])
-                
-    pprint(new_High_textSetsLIST)
-    print(len(new_High_textSetsLIST))
-    pprint(new_Low_textSetsLIST)
-    print(len(new_Low_textSetsLIST))
-    
+
+    #pprint(new_High_textSetsLIST)
+    #print(len(new_High_textSetsLIST))
+    #pprint(new_Low_textSetsLIST)
+    #print(len(new_Low_textSetsLIST))
+
     # Combine the High & Low texts into one LIST
     total_stimSetLIST.extend(new_High_textSetsLIST)
     total_stimSetLIST.extend(new_Low_textSetsLIST)
-    
+
     # Shuffle the texts so that the texts would be randomized
     random.shuffle(total_stimSetLIST)
-    
-    
+
+
     # Experiment section
     # Reading Comprehension Task STARTS
-    for i in total_stimSetLIST:
+    for i in total_stimSetLIST:  #total_stimSetLIST[:3]:  
         # display instructions for Reading Comprehension phase
         display_ins(instructions_1, keypress)
         #win.flip()
         core.wait(0.5)
-        
+
         # display fixation in the central of the screen
         display_fix()
         core.wait(1)
-        
+
         start_time = clock.getTime()
         # display the stimuli, which would be a series of short texts
         testing_text = i
         text = visual.TextStim(win = win, text = testing_text)
         print("text starts")
         text.draw()
+
+        # TO MARK THE AUDIO FILE BEGINS
+        port.setData(2) #This is open the trigger
+        core.wait(0.01) # Stay for 10 ms
+        port.setData(0) #This is close the trigger
+
         win.flip()
-        
+
         # adding rating scale in here!!!!
-        
+
         # setting up what keypress would allow the experiment to proceed
         keys = event.waitKeys(keyList = keypress)
         event.getKeys(keyList = keypress)
@@ -254,7 +260,12 @@ if __name__ == "__main__":
             clock.reset()
         else:
             pass
-        
+
+        # TO MARK THE AUDIO FILE ENDS
+        port.setData(2) #This is open the trigger
+        core.wait(0.01) # Stay for 10 ms
+        port.setData(0) #This is close the trigger
+
         # making the wanted info into the List form for future use
         text_noLIST.append(int(total_stimSetLIST.index(i))+1)
         dateLIST.append(day)
@@ -263,16 +274,16 @@ if __name__ == "__main__":
         self_paced_rtLIST.append(time_duration)
         shuffledTotalT_LIST.append([i])
         rating_LIST.append([""])
-        
+
         core.wait(0.5)
-        
+
         # ask the participant to evaluate how well they understand the presented text
         display_ins(instructions_2, keypress)
-        
-        
+
+
     # close the window  at the end of the experiment
     win.close()
-    
+
     # Saving the self_paced_rt result into csv file
     dataDICT = pd.DataFrame({'Sub_id':sub_idLIST,
                        'Date':dateLIST,
@@ -281,35 +292,35 @@ if __name__ == "__main__":
                        'Rating Scale': rating_LIST,
                        'Text content': shuffledTotalT_LIST
                        })
-    
+
     pseudoDICT = {"The TargetPseudo group_6":targetPseudoLIST,
                   "The ControlPseudo group_6": controlPseudoLIST,
                   "High_CD condition pseudowords_3":words_high_CD_setLIST,
                   "Low_CD condition pseudowords_3":words_low_CD_setLIST}
-    
+
     textsDICT = {"The High-Low Set Group": Setsinfo_LIST,
                  "High_textSetsLIST": new_High_textSetsLIST,
                  "Low_textSetsLIST":new_Low_textSetsLIST,
                  "Total_stimSetLIST":total_stimSetLIST}
-    
-    
+
+
     #print(type(dataDICT))
-    
+
     #data_path = "/Users/ting-hsin/Docs/Github/ICN_related"
     file_name = sub_id + '_Reading_task.csv'
     fsave_path = result_data_path + file_name
     dataDICT.to_csv(fsave_path, sep = "," ,index = False , header = True, encoding = "UTF-8")
-    
+
     DICT_name = sub_id + '_pseudowordsDICT.json'
     Dsave_path = result_data_path + DICT_name
     with open(Dsave_path, "w", newline='', encoding="UTF-8") as jsfile:
         json.dump(pseudoDICT, jsfile, ensure_ascii=False)
-        
+
     Text_name = sub_id + '_textsDICT.json'
     Tsave_path = result_data_path + Text_name
     with open(Tsave_path, "w", newline='', encoding="UTF-8") as jsfile_2:
         json.dump(textsDICT, jsfile_2, ensure_ascii=False)
-        
+
 
     # close all the possible ongoing commands that could be running in the background
     core.quit()  # normally we would add it, in case that anything happen
