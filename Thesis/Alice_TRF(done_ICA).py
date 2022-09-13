ba3a77b555d105f07475b9731ab0175b2bd257dc
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-09-13 09:41:42 +0800

    Setting up the TRF needed packages

---
+++ Thesis/Alice_TRF(done_ICA).py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+# The normal packages for nlp
+from pprint import pprint
+import csv
+import json
+import random
+from random import sample
+from datetime import datetime,date
+import pandas as pd
+from collections import Counter
+
+# TRF required packages
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import scipy.io as sio
+from scipy import stats
+import numpy as np
+import mne
+import eelbrain
+from itertools import islice
+
+
+
+if __name__ == "__main__":
