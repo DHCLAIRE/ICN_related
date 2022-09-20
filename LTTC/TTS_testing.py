c2ec59cf112e9fe3ccced61234ea89ba9a08d345
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-09-20 14:33:00 +0800

    方法2_gTTS可行但會斷在奇怪的地方，換方法3_pyttsx3

--- LTTC/TTS_testing.py
+++ LTTC/TTS_testing.py
@@ -15,13 +15,45 @@ from gtts import gTTS
 # play the converted audio
 import os
 
+import pyttsx3
+
 
 if __name__ == "__main__":
     
+    data_path  = "/Users/neuroling/Downloads/"
 
+    # 初始化
+    engine = pyttsx3.init()
     
+    voices = engine.getProperty('voices')
+    # 語速控制
+    rate = engine.getProperty('rate')
+    print(rate)
+    engine.setProperty('rate', rate-20)
+    
+    # 音量控制
+    volume = engine.getProperty('volume')
+    print(volume)
+    engine.setProperty('volume', volume-0.25)
+    
+    engine.say('hello world')
+    engine.say('你好，世界')
+    # 朗讀一次
+    engine.runAndWait()
+    
+    engine.say('語音合成開始')
+    engine.say('我會說中文了，開森，開森')
+    engine.runAndWait()
+    
+    engine.say('The quick brown fox jumped over the lazy dog.')
+    engine.runAndWait()
+    
+    
+    
+    """
+    # GOOGLE gTTS >> worked but pause at the strange point  >> Try other's method first
     # The text that you want to convert to audio
-    mytext = 'Welcome to geeksforgeeks!'
+    mytext = "Danny drops out of chemistry after the first quarter and graduates with honors in jazz performance and composition. Students learn how Danny has gone on to a very successful career as a jazz pianist and composer. Slow Starters is another story that many students find relevant to their own lives. It tells the story of a young man, Ari, who spent five years in and out of community college while working part-time jobs. Not until Ari met the 'girl of his dreams' did he begin taking a serious interest in finding a career path. He transferred to a four-year university and chose physical education as a major. After graduation Ari was accepted into an osteopathic medical school and became a doctor. And yes, he marry that dream girl. Creativity and Courage is yet another inspiring story about a Japanese graduate student who started his doctoral baepay research in my laboratory."
     
     # Language in which you want to convert
     language = 'en'
@@ -30,16 +62,18 @@ if __name__ == "__main__":
     # here we have marked slow=False. Which tells 
     # the module that the converted audio should 
     # have a high speed
-    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj = gTTS(text = mytext, lang = language, slow = False)
     
     # Saving the converted audio in a mp3 file named
     # welcome 
-    myobj.save("welcome.mp3")
+    myobj.save(data_path + "Text_Test1.mp3")
     
     # Playing the converted file
-    os.system("mpg321 welcome.mp3")
+    #os.system("mpg321 welcome.mp3")
     
     
+    
+    """
     """
     ## FAIL ##
     url = "https://voicerss-text-to-speech.p.rapidapi.com/"
