a2524e40986e72b8c5ebe8dd670d1fd8eb3315ab
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-09-20 13:58:49 +0800

    (失敗)方法1_API unsubscribed，新增方法2_gTTS

--- LTTC/TTS_testing.py
+++ LTTC/TTS_testing.py
@@ -5,10 +5,43 @@
 
 # This is the part that should be able to do the TTS (Text-to-speech)
 
-import requests
+#import requests
+
+# Import the required module for text 
+# to speech conversion
+from gtts import gTTS
+
+# This module is imported so that we can 
+# play the converted audio
+import os
 
 
 if __name__ == "__main__":
+    
+
+    
+    # The text that you want to convert to audio
+    mytext = 'Welcome to geeksforgeeks!'
+    
+    # Language in which you want to convert
+    language = 'en'
+    
+    # Passing the text and language to the engine, 
+    # here we have marked slow=False. Which tells 
+    # the module that the converted audio should 
+    # have a high speed
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    
+    # Saving the converted audio in a mp3 file named
+    # welcome 
+    myobj.save("welcome.mp3")
+    
+    # Playing the converted file
+    os.system("mpg321 welcome.mp3")
+    
+    
+    """
+    ## FAIL ##
     url = "https://voicerss-text-to-speech.p.rapidapi.com/"
     
     querystring = {"key":"undefined"}
@@ -23,3 +56,5 @@ if __name__ == "__main__":
     response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
     
     print(response.text)
+    
+    """
