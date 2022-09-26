c972b18b6e7af58a3b901e40b7ec21b065eb2073
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-09-26 10:52:22 +0800

    刪除廢碼

--- LTTC/LTTC_Reading.py
+++ LTTC/LTTC_Reading.py
@@ -310,26 +310,3 @@ if __name__ == "__main__":
 
     # close all the possible ongoing commands that could be running in the background
     core.quit()  # normally we would add it, in case that anything happen
-
-
-
-"""
-# This is the part that should be able to do the TTS (Text-to-speech)
-
-import requests
-
-url = "https://voicerss-text-to-speech.p.rapidapi.com/"
-
-querystring = {"key":"undefined"}
-
-payload = "src=Hello%2C%20world!&hl=en-us&r=0&c=mp3&f=8khz_8bit_mono"
-headers = {
-	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-	"X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com"
-}
-
-response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-
-print(response.text)
-"""
