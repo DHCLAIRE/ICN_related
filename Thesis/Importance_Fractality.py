fe7aebb2dc9e60326d3553e9a5e32a96fae2f408
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-10-13 15:28:50 +0800

    修改結果顯示的說明內容

--- Thesis/Importance_Fractality.py
+++ Thesis/Importance_Fractality.py
@@ -311,14 +311,14 @@ if __name__ == "__main__":
             
             # Access the elements of boxed list based on the box size
             for small_boxLIST in boxedLIST:
-                print("small_boxLIST = ", small_boxLIST)  # The model only catchs the last small_boxLIST, there for the word count might be wrong
+                #print("small_boxLIST = ", small_boxLIST)  # The model only catchs the last small_boxLIST, there for the word count might be wrong
                 
                 # Count the presence of the target word
                 if wordSTR in small_boxLIST:
                     t_wordcountINT +=1
                     
                     #print(wordSTR)
-            print("The word count of", wordSTR, "in box size = ", len(boxedLIST[0]), "is", t_wordcountINT)
+            print("The word count of", "[", wordSTR, "]", "in box size = ", "[", len(boxedLIST[0]), "]", "is", t_wordcountINT)
             print(type(t_wordcountINT))
             # The count in this section would need to be DICTed by the box size, I think
                 #else:
