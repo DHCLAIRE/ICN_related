05d20b4cb9728ea1977416232ca270e7c8cfe22d
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-10-20 10:52:50 +0800

    測試loop下數字組合成的字串指令

--- LTTC/Testing.py
+++ LTTC/Testing.py
@@ -5,7 +5,7 @@ import csv
 from pprint import pprint
 
 if __name__ == "__main__":
-
+    """
     result_data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_MEG_S001/"  # Testing
     sub_id = str(input("Subject: "))
     
@@ -13,3 +13,10 @@ if __name__ == "__main__":
     with open(result_data_path + "S%s_Reading_task.csv" %sub_id, "r", encoding="UTF-8") as csvfile:  #, newline=''
         result_csvLIST = csvfile.read().split("\n")
         pprint(result_csvLIST)
+    """
+    
+    for i in range(2):
+        for k in range(3):
+            numSTR = str(int("%d%d" %(i, k))+1)
+            #numSTR = "%02d" %(i, k)
+            print(numSTR)
