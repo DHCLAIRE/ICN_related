7496ab56f5d77f03c0c2d777a32f896ed8f79f76
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-10-24 21:34:07 +0800

    (轉wav測試未成功)修改變數名稱

--- LTTC/mp3towav.py
+++ LTTC/mp3towav.py
@@ -9,20 +9,20 @@ import subprocess
 
 if __name__ == "__main__":
     
-    data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_MEG_S002/S002_audios/"
+    data_path = "/Users/ting-hsin/Docs/Github/ICN_related/LTTC/S002_audios/"
     
     # files
-    #src = data_path + "S002_textaudio_1.mp3"
-    #dst = "S002_test_1.wav"
-    
+    src = data_path + "S002_textaudio_1.mp3"
+    dst = "S002_test_1.wav"
+    """
     ## convert wav to mp3
-    #audSeg = AudioSegment.from_mp3(data_path+"S002_textaudio_1.mp3")
-    #audSeg.export(dst, format="wav")
+    audSeg = AudioSegment.from_mp3(src)
+    audSeg.export(dst, format="wav")
     
     ##open data to check the sampling rate
-    #sample_rate, data = wavfile.read(data_path + 'S002_test_1.wav') #.format(i+1))
-    #print(sample_rate)
-    ##print("The data points of tape 1 is", len(data))
-    
-    subprocess.call(['ffmpeg', '-i', data_path + "S002_textaudio_1.mp3",data_path + 'S002_test_1.wav'])
+    sample_rate, data = wavfile.read(dst) #.format(i+1))
+    print(sample_rate)
+    print("The data points of tape 1 is", len(data))
+    """
+    subprocess.call(['ffmpeg', '-i', src, dst])
     
