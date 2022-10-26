afbfee1d018f36cbdebe6bd138fd5eb3becda22b
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-10-26 21:41:26 +0800

    查看各種類的音檔dtype

--- LTTC/Testing.py
+++ LTTC/Testing.py
@@ -4,7 +4,28 @@
 import csv
 from pprint import pprint
 
+from psychopy import prefs
+prefs.hardware['audioLib'] = ['PTB', 'pyo', 'pygame']
+
+import psychtoolbox as ptb
+from psychopy import sound, core, visual, event, gui, monitors, clock, parallel  #, parallel   # if you change the setting, this command must be put after the prefs's command
+#import json
+print(sound.Sound)
+
+
+import scipy
+from scipy.io import wavfile
+import numpy as np
+from datetime import datetime,date
+import json
+import numpy as np
+import pandas as pd
+from pprint import pprint
+
+
+
 if __name__ == "__main__":
+    """
     sub_id = str(input("Subject: "))
     
     result_data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_MEG_S%s/" %sub_id # Testing
@@ -12,10 +33,32 @@ if __name__ == "__main__":
     with open(result_data_path + "S%s_Reading_task.csv" %sub_id, "r", encoding="UTF-8") as csvfile:  #, newline=''
         result_csvLIST = csvfile.read().split("\n")
         pprint(result_csvLIST)
+    """
     
+    # Inspecting the dtype of the wav file
+    data_path = "/Volumes/Neurolang_1/Master Program/New_Thesis_topic/Alice(EEG dataset_mat_and stimuli)/audio/"
+    data_path_1 = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_MEG_S001/S001_audios/"
     
+    LTTC_stm_old = data_path_1 + "S001_textaudio_modified_1.wav"
+    LTTC_stm_new = data_path_1 + "S001_modified_1.wav"
+    Alice_stm = data_path + "DownTheRabbitHoleFinal_SoundFile1.wav"
     
-    
+    sample_rate, data = wavfile.read(Alice_stm)
+    sample_rate_1, data_1 = wavfile.read(LTTC_stm_old)
+    sample_rate_2, data_2 = wavfile.read(LTTC_stm_new)
+    print("Alice sampling_R=",sample_rate)
+    print("LTTC old sampling_R=",sample_rate_1)
+    print("LTTC new sampling_R=",sample_rate_2)
+    #print(data)
+    print(type(data))
+    print(type(data_1))
+    print(type(data_2))
+    x = data.dtype
+    y = data_1.dtype
+    z = data_2.dtype
+    print(x)
+    print(y)
+    print(z)
     
     """
     # How to create a string in a 2 layers loop
