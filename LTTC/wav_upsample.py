a7f2391952f086f201052046c1962227dd386f94
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-11-01 09:22:48 +0800

    修改路徑和檔名upsample新的音檔

--- LTTC/wav_upsample.py
+++ LTTC/wav_upsample.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     # The original script
     # https://gist.github.com/alexjaw/09af24d58ac99e1e4cafba092e063fe3
     
-    data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_MEG_S001/S001_audios/"
+    data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_MEG_S999/"
     
     new_fs = 44100
     
-    for i in range(1):
+    for i in range(7):
         # open data
-        sample_rate, data = wavfile.read(data_path + 'S001_textaudio_modified_{}.wav'.format(i+1))
+        sample_rate, data = wavfile.read(data_path + 'S999_textaudio_{}.wav'.format(i+1))
     
         print(sample_rate)
         print("The data points of tape", i+1,"is" ,len(data))
@@ -29,11 +29,11 @@ if __name__ == "__main__":
         value = data[-1]  # what does this means??   # data[-1]== -1, <class 'numpy.int16'>
         new_data = np.append(data, value)
         
-        """
-        wavfile.write(filename=data_path+"S001_modified_{}.wav".format(i+1), rate=44100, data=new_data)
         
+        wavfile.write(filename=data_path+"S999_modified_{}.wav".format(i+1), rate=44100, data=new_data)
+        """
         # open NEW data
-        sample_rate_new, data_new = wavfile.read(data_path + "S001_modified_{}.wav".format(i+1))
+        sample_rate_new, data_new = wavfile.read(data_path + "S999_modified_{}.wav".format(i+1))
     
         print(sample_rate_new)
         print("The NEW data points of tape", i+1,"is" ,len(data_new))
