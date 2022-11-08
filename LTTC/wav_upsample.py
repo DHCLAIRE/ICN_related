0b64bdc82c6f49b1cdb868a4d38eaf3bd421f991
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-11-09 00:17:01 +0800

    修改假詞的sampling frequency

--- LTTC/wav_upsample.py
+++ LTTC/wav_upsample.py
@@ -11,11 +11,30 @@ if __name__ == "__main__":
     # https://gist.github.com/alexjaw/09af24d58ac99e1e4cafba092e063fe3
     
     data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_MEG_S002/S002_audios/"
+    stim_data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_LDT_pw_audios/"
     
     new_fs = 44100
     
     
+    pseudowordLIST = ["aegliy", "baepay", "baydiy", "browmey", "chaeviy", "laelaxst", "laeviy", "maeskiy", "paenliy", "payliy", "vaesow", "weyaet"]
+    for wordSTR in pseudowordLIST:
+        # open data
+        sample_rate, data = wavfile.read(stim_data_path + '{}_v3_female.wav'.format(wordSTR))
     
+        print(sample_rate)
+        print("The data points of tape", wordSTR,"is" ,len(data))
+        
+        # resample data
+        new_num_samples = round(len(data)*float(new_fs)/sample_rate)
+        data = scipy.signal.resample(data, new_num_samples).astype(np.int16)  # no astype(np.int16)'s dtype == float64
+        #data_dtype = data.dtype
+        #print(data_dtype)
+        value = data[-1]  # what does this means??   # data[-1]== -1, <class 'numpy.int16'>
+        new_data = np.append(data, value)
+        
+        
+        wavfile.write(filename=stim_data_path+'{}_v3_female.wav'.format(wordSTR), rate=44100, data=new_data)    
+    '''
     for i in range(30):
         # open data
         sample_rate, data = wavfile.read(data_path + 'S002_textaudio_modified_{}.wav'.format(i+1))
@@ -33,7 +52,8 @@ if __name__ == "__main__":
         
         
         wavfile.write(filename=data_path+"S002_modified_{}.wav".format(i+1), rate=44100, data=new_data)
-        """
+        '''
+    """
         # open NEW data
         sample_rate_new, data_new = wavfile.read(data_path + "S999_modified_{}.wav".format(i+1))
     
