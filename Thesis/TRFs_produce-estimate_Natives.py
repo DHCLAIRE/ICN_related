df48d4baa458d638206b91e71a4f3d16ab450cca
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-11-22 10:23:40 +0800

    修改路徑名及檔名以執行計算出含參數的TRFs

--- Thesis/TRFs_produce-estimate_Natives.py
+++ Thesis/TRFs_produce-estimate_Natives.py
@@ -12,37 +12,45 @@ import trftools
 if __name__ == "__main__":
     
     STIMULI = [str(i) for i in range(1, 13)]
-    DATA_ROOT = Path("~").expanduser() / 'Data' / 'Alice'
-    PREDICTOR_DIR = DATA_ROOT / 'predictors'
-    EEG_DIR = DATA_ROOT / 'eeg'
+    DATA_ROOT = Path("/Volumes/Neurolang_1/Master Program/New_Thesis_topic/Experiments_Results")  #Path("~").expanduser() / 'Data' / 'Alice'
+    PREDICTOR_audio_DIR = DATA_ROOT / 'TRFs_pridictors/audio_predictors'
+    PREDICTOR_word_DIR = DATA_ROOT / 'TRFs_pridictors/word_predictors'
+    EEG_DIR = DATA_ROOT / 'EEG_Natives'
     SUBJECTS = [path.name for path in EEG_DIR.iterdir() if re.match(r'S\d*', path.name)]
     # Define a target directory for TRF estimates and make sure the directory is created
-    TRF_DIR = DATA_ROOT / 'TRFs'
+    TRF_DIR = DATA_ROOT / 'TRFs_Natives'
     TRF_DIR.mkdir(exist_ok=True)
+    #print(SUBJECTS)
     
     # Load stimuli
     # ------------
     # Make sure to name the stimuli so that the TRFs can later be distinguished
     # Load the gammatone-spectrograms; use the time axis of these as reference
-    gammatone = [eelbrain.load.unpickle(PREDICTOR_DIR / f'{stimulus}~gammatone-8.pickle') for stimulus in STIMULI]
+    gammatone = [eelbrain.load.unpickle(PREDICTOR_audio_DIR / f'{stimulus}~gammatone-8.pickle') for stimulus in STIMULI]
+    
     # Resample the spectrograms to 100 Hz (time-step = 0.01 s), which we will use for TRFs
     gammatone = [x.bin(0.01, dim='time', label='start') for x in gammatone]
+    
     # Pad onset with 100 ms and offset with 1 second; make sure to give the predictor a unique name as that will make it easier to identify the TRF later
     gammatone = [trftools.pad(x, tstart=-0.100, tstop=x.time.tstop + 1, name='gammatone') for x in gammatone]
+    
     # Load the broad-band envelope and process it in the same way
-    envelope = [eelbrain.load.unpickle(PREDICTOR_DIR / f'{stimulus}~gammatone-1.pickle') for stimulus in STIMULI]
+    envelope = [eelbrain.load.unpickle(PREDICTOR_audio_DIR / f'{stimulus}~gammatone-1.pickle') for stimulus in STIMULI]  # Load in the data
     envelope = [x.bin(0.01, dim='time', label='start') for x in envelope]
     envelope = [trftools.pad(x, tstart=-0.100, tstop=x.time.tstop + 1, name='envelope') for x in envelope]
-    onset_envelope = [eelbrain.load.unpickle(PREDICTOR_DIR / f'{stimulus}~gammatone-on-1.pickle') for stimulus in STIMULI]
+    onset_envelope = [eelbrain.load.unpickle(PREDICTOR_audio_DIR / f'{stimulus}~gammatone-on-1.pickle') for stimulus in STIMULI]
     onset_envelope = [x.bin(0.01, dim='time', label='start') for x in onset_envelope]
     onset_envelope = [trftools.pad(x, tstart=-0.100, tstop=x.time.tstop + 1, name='onset') for x in onset_envelope]
+    
     # Load onset spectrograms and make sure the time dimension is equal to the gammatone spectrograms
-    gammatone_onsets = [eelbrain.load.unpickle(PREDICTOR_DIR / f'{stimulus}~gammatone-on-8.pickle') for stimulus in STIMULI]
+    gammatone_onsets = [eelbrain.load.unpickle(PREDICTOR_audio_DIR / f'{stimulus}~gammatone-on-8.pickle') for stimulus in STIMULI]
     gammatone_onsets = [x.bin(0.01, dim='time', label='start') for x in gammatone_onsets]
     gammatone_onsets = [eelbrain.set_time(x, gt.time, name='gammatone_on') for x, gt in zip(gammatone_onsets, gammatone)]
+    
     # Load word tables and convert tables into continuous time-series with matching time dimension
-    word_tables = [eelbrain.load.unpickle(PREDICTOR_DIR / f'{stimulus}~word.pickle') for stimulus in STIMULI]
+    word_tables = [eelbrain.load.unpickle(PREDICTOR_word_DIR / f'{stimulus}~Ngram-CFG_word.pickle') for stimulus in STIMULI]
     word_onsets = [eelbrain.event_impulse_predictor(gt.time, ds=ds, name='word') for gt, ds in zip(gammatone, word_tables)]
+    
     # Function and content word impulses based on the boolean variables in the word-tables
     word_lexical = [eelbrain.event_impulse_predictor(gt.time, value='lexical', ds=ds, name='lexical') for gt, ds in zip(gammatone, word_tables)]
     word_nlexical = [eelbrain.event_impulse_predictor(gt.time, value='nlexical', ds=ds, name='non_lexical') for gt, ds in zip(gammatone, word_tables)]
