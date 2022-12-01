2f0ed4ad1928351c63ba737cbea3811a8604d0b7
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2022-12-01 10:20:38 +0800

    新增假詞的替換字母DICT

--- LTTC/TTS_testing.py
+++ LTTC/TTS_testing.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     # here we have marked slow=False. Which tells 
     # the module that the converted audio should 
     # have a high speed
-    myobj = gTTS(text = mytext, lang = language, slow = False)
+    myobj = gTTS(text=mytext, lang=language, slow=False)
     
     # Saving the converted audio in a mp3 file named
     # welcome 
@@ -124,3 +124,17 @@ if __name__ == "__main__":
     print(response.text)
     
     """
+    
+    replace_pwDICT = {'aeggli':'aeggli',
+                      'baepay':'badpie',
+                      'baydiy':'bidey',
+                      'browmey':'bromay',
+                      'chaeviy':'chavi',
+                      'laelaxst':'lalust',
+                      'laeviy':'lavvi',
+                      'maeskiy':'masgi',
+                      'paenliy':'panli',
+                      'payliy':'piely',
+                      'vaesow':'vasle',
+                      'weyaet':'way-at'}
+    print(type(replace_pwDICT.get('paenliy')))
