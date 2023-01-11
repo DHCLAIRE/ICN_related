8dcbc15f6dbacc4277d45b0722b6f7a7f43b557b
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-01-11 11:14:40 +0800

    更改路徑以upsample LTTC聽力行為的所有音檔

--- LTTC/wav_upsample.py
+++ LTTC/wav_upsample.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     # https://gist.github.com/alexjaw/09af24d58ac99e1e4cafba092e063fe3
     root_data_path  = Path("/Users/ting-hsin/Downloads/S005-S007/")
     
-    data_path = Path("/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG")
+    data_path = Path("/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_audio_behavioral")
     #stim_data_path = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/LTTC_LDT_pw_audios/"
     
     new_fs = 44100
@@ -38,13 +38,14 @@ if __name__ == "__main__":
         wavfile.write(filename=stim_data_path+'{}_v3_female.wav'.format(wordSTR), rate=44100, data=new_data)    
     """
     
-    for subj in range(24, 26):
+    for subj in range(2, 31):
         subj_id = '%.3d' %subj
-        audio_data_path = data_path / Path("LTTC_MEG_S%s/S%s_audios" %(subj_id, subj_id))
+        audio_data_path = data_path / Path("LTTC_Behavioral_S%s" %subj_id)  # tmp, delete this command later
+        #audio_data_path = data_path / Path("LTTC_Behavioral_S%s/S%s_behavioral_audios" %(subj_id, subj_id))
         print(subj_id)
         for i in range(30):
             # open data
-            sample_rate, data = wavfile.read(audio_data_path / Path('S{}_textaudio_{}.wav'.format(subj_id, i+1)))
+            sample_rate, data = wavfile.read(audio_data_path / Path('S{}_behavioral_textaudio_{}.wav'.format(subj_id, i+1)))
             
             print(sample_rate)
             print("The data points of tape", i+1,"is" ,len(data))
@@ -58,9 +59,9 @@ if __name__ == "__main__":
             new_data = np.append(data, value)
         
         
-            wavfile.write(filename=audio_data_path / Path("S{}_textaudio_{}.wav".format(subj_id, i+1)), rate=44100, data=new_data)
-        
-    
+            wavfile.write(filename=audio_data_path / Path("S{}_behavioral_textaudio_{}.wav".format(subj_id, i+1)), rate=44100, data=new_data)
+        print("S%s" %subj_id, "is DONE!")
+    print("ALL DONEEE!!")
         # open NEW data
         #sample_rate_new, data_new = wavfile.read(data_path + "S999_modified_{}.wav".format(i+1))
     
