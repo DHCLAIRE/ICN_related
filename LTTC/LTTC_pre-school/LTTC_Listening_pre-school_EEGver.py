b5365aa3edcc89b2de63a8acb735b97b863d15e3
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-03-02 09:05:08 +0800

    刪除廢碼、標註1min-break的註解

--- LTTC/LTTC_pre-school/LTTC_Listening_pre-school_EEGver.py
+++ LTTC/LTTC_pre-school/LTTC_Listening_pre-school_EEGver.py
@@ -200,14 +200,11 @@ if __name__ == "__main__":
             dateLIST.append(day)
             responseLIST.append(ans_keypressSTR)
 
-            # the Gap between each audio files
-            #core.wait(5)
-            #print("Continue for the SoundFile{}".format(int(tape_numSTR)+1))
         # ask the participant to evaluate how well they understand the presented text
         if i == 3:
             display_ins(instructions_4, keypressLIST_space)  # End of experiment
         else:
-            display_ins(instructions_3, keypressLIST_space)  # 1 miute break
+            display_ins(instructions_3, keypressLIST_space)  # 1 miute break == the Gap between each audio files
 
 
     print("FINISHIED!")
