0d7534708db6cff2920e008888398bce2e619f7c
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-04-11 15:45:40 +0800

    未修改，僅確認predictors的長度

--- Thesis/TRFs_produce-estimate_Natives.py
+++ Thesis/TRFs_produce-estimate_Natives.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     
     # Pad onset with 100 ms and offset with 1 second; make sure to give the predictor a unique name as that will make it easier to identify the TRF later
     gammatone = [trftools.pad(x, tstart=-0.100, tstop=x.time.tstop + 1, name='gammatone') for x in gammatone]
-    """
+    
     # Load the broad-band envelope and process it in the same way
     envelope = [eelbrain.load.unpickle(PREDICTOR_audio_DIR / f'{stimulus}~gammatone-1.pickle') for stimulus in STIMULI]  # Load in the data
     envelope = [x.bin(0.01, dim='time', label='start') for x in envelope]
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     gammatone_onsets = [eelbrain.load.unpickle(PREDICTOR_audio_DIR / f'{stimulus}~gammatone-on-8.pickle') for stimulus in STIMULI]
     gammatone_onsets = [x.bin(0.01, dim='time', label='start') for x in gammatone_onsets]
     gammatone_onsets = [eelbrain.set_time(x, gt.time, name='gammatone_on') for x, gt in zip(gammatone_onsets, gammatone)]
-    """
+    
     # Load word tables and convert tables into continuous time-series with matching time dimension
     word_tables = [eelbrain.load.unpickle(PREDICTOR_word_DIR / f'{stimulus}~Ngram-CFG_word.pickle') for stimulus in STIMULI]
     word_onsets = [eelbrain.event_impulse_predictor(gt.time, ds=ds, name='word') for gt, ds in zip(gammatone, word_tables)] # not sure why they could get the word onset this way
@@ -66,6 +66,15 @@ if __name__ == "__main__":
     # ------
     # Pre-define models here to have easier access during estimation. In the future, additional models could be added here and the script re-run to generate additional TRFs.
     models = {
+        # Acoustic models
+        'envelope': [envelope],
+        'envelope+onset': [envelope, onset_envelope],
+        'acoustic': [gammatone, gammatone_onsets],
+        # Models with word-onsets and word-class
+        'words': [word_onsets],
+        'words+lexical': [word_onsets, word_lexical, word_nlexical],
+        'acoustic+words': [gammatone, gammatone_onsets, word_onsets],
+        'acoustic+words+lexical': [gammatone, gammatone_onsets, word_onsets, word_lexical, word_nlexical],
         # Language Models
         'Ngram': [word_Ngram],
         'CFG': [word_CFG],
@@ -118,7 +127,9 @@ if __name__ == "__main__":
             # Select and concetenate the predictors corresponding to the EEG trials
             predictors_concatenated = []
             for predictor in predictors:
+                print(predictor)
                 predictors_concatenated.append(eelbrain.concatenate([predictor[i] for i in trial_indexes]))
+            print(predictors_concatenated)
             # Fit the mTRF
             trf = eelbrain.boosting(eeg_concatenated, predictors_concatenated, -0.100, 1.000, error='l1', basis=0.050, partitions=5, test=1, selective_stopping=True)
             # Save the TRF for later analysis
