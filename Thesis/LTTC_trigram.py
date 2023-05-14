9f7c1c1c5e363324d6c1f0f77cfb563dc401c950
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-14 17:40:37 +0800

    (Untested)Add ctrl+CV's trigram model

--- Thesis/LTTC_trigram.py
+++ Thesis/LTTC_trigram.py
@@ -16,6 +16,11 @@ import re
 from nltk import sent_tokenize
 from nltk import tokenize
 
+# Using Reuters as the corpus to train a trigram model
+from nltk.corpus import reuters
+from nltk import bigrams, trigrams
+from collections import Counter, defaultdict
+
 
 def LISTblankEraser(rawLIST):
     '''
@@ -32,4 +37,18 @@ def LISTblankEraser(rawLIST):
     return newrawLIST
 
 if __name__ == "__main__":
+
     
+    # Create a placeholder for model
+    model = defaultdict(lambda: defaultdict(lambda: 0))
+    
+    # Count frequency of co-occurance  
+    for sentence in reuters.sents():
+        for w1, w2, w3 in trigrams(sentence, pad_right=True, pad_left=True):
+            model[(w1, w2)][w3] += 1
+    
+    # Let's transform the counts to probabilities
+    for w1_w2 in model:
+        total_count = float(sum(model[w1_w2].values()))
+        for w3 in model[w1_w2]:
+            model[w1_w2][w3] /= total_count    
