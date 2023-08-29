a23adde9f4b18db953d7ab277ca92840f5f8d75f
dinghsin12345@gmail.com <54883032+DHCLAIRE@users.noreply.github.com>
2023-08-30 00:38:26 +0300

    Update textgrid2csv_testing.py

--- LTTC/LTTC_preschool/textgrid2csv_testing.py
+++ LTTC/LTTC_preschool/textgrid2csv_testing.py
@@ -24,15 +24,69 @@ def inputtextlines(filename):
     return linelist
 
 def outputtext(filename, text):
-    handle = open(filename,'w')
+    handle = open(filename,'w')  #, encoding="utf-8"
     handle.write(text)
     handle.close()
-    
-    
+
+#-------------------------------------------------
+# Conversion routines
+
+def converttextgrid2csv(textgridlines,textgridname):
+
+    csvtext = '# TextGrid to CSV (D. Gibbon, 2008-11-23)\n# Open the file with OpenOffice.org Calc or MS-Excel.\nFileName\tTierType\tTierName\tLabel\tStart\tEnd\tDuration\n'
+
+    newtier = False
+    for line in textgridlines[9:]:
+        line = re.sub('\n','',line)
+        line = re.sub('^ *','',line)
+        linepair = line.split(' = ')
+        if len(linepair) == 2:
+            if linepair[0] == 'class':
+                classname = linepair[1]
+            if linepair[0] == 'name':
+                tiername = linepair[1]
+            if linepair[0] == 'xmin':
+                xmin = linepair[1]
+            if linepair[0] == 'xmax':
+                xmax = linepair[1]
+            if linepair[0] == 'text':
+                text = linepair[1]
+                diff = str(float(xmax)-float(xmin))
+                csvtext += textgridname + '\t' + classname + '\t' + tiername + '\t' + text + '\t' + xmin + '\t' + xmax + '\t' + diff + '\n'
+    return csvtext
+
+#-------------------------------------------------
+# Main caller
+"""
+def main():
+
+    if len(sys.argv) < 2:
+        print("Usage: textgrid2csv.py <textgridfilename>")
+        exit()
+    textgridname = sys.argv[1]
+    csvname = textgridname + '.csv'
+
+    textgrid = inputtextlines(textgridname)
+
+    textcsv = converttextgrid2csv(textgrid,textgridname)
+
+    outputtext(csvname,textcsv)
+
+    print("Output file: " + csvname)
+
+    return
+"""
 if __name__ == "__main__":
     
-    textgrid_path = "/Users/ting-hsin/Downloads/"
+    textgrid_path = "/Users/ting-hsin/Downloads/Sound/"
     
-    textgridname = textgrid_path + "f051p003-10.TextGrid"
+    textgridname = textgrid_path + "story1.TextGrid"
+    csvname = textgridname + '.csv'
+
     textgrid = inputtextlines(textgridname)
-    #pprint(textgrid)
+
+    textcsv = converttextgrid2csv(textgrid,textgridname)
+
+    outputtext(csvname,textcsv)
+
+    print("Output file: " + csvname)
