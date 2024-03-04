dc35aac4ac47d378f385113297f4cc2a1b7574ee
dinghsin12345@gmail.com <54883032+DHCLAIRE@users.noreply.github.com>
2024-03-04 22:05:12 +0200

    Add self-defined FUNC for determine groups types

--- Rename_file_name.py
+++ Rename_file_name.py
@@ -17,6 +17,56 @@ import re
 from pathlib import Path
 import serial
 
+
+def gender(inputSTR):
+    '''
+    Determine the gender category
+    '''
+    sub_genderSTR = str()
+    if inputSTR == "female":
+        sub_genderSTR = "F"
+    if inputSTR == "male":
+        sub_genderSTR = "M"
+    else:
+        print("No gender")
+    
+    return sub_genderSTR
+
+def race(inputSTR):
+    '''
+    Determine the race category
+    '''
+    sub_raceSTR = str()
+    if inputSTR == "black":
+        sub_raceSTR = "B"
+    if inputSTR == "caucasian":
+        sub_raceSTR = "W"
+    if inputSTR == "asian":
+        sub_raceSTR = "A"
+    else:
+        print("No race")
+    
+    return sub_raceSTR
+
+def emotion(inputSTR):
+    '''
+    Determine the emotion category
+    '''
+    sub_emoSTR = str()
+    if inputSTR == "angry":
+        sub_emoSTR = "angry"
+    if inputSTR == "fearful":
+        sub_emoSTR = "fearful"
+    if inputSTR == "neutral":
+        sub_emoSTR = "neutral"
+    else:
+        print("No emotion")
+    
+    return sub_emoSTR
+
+
+
+
 if __name__ == "__main__":
     # Set up the data path (For Mac)
     root_data_path = Path("/Users/ting-hsin/Downloads/MaterialsExp2")
@@ -36,11 +86,11 @@ if __name__ == "__main__":
         
         # Extract the pic name out of the data path
         pic_pathSTR = pic_infoLIST[0]
-        print(pic_pathSTR)
+        
         # To change the backward slash('\\') into forward slash('/'), so that the Path(datapath) may be easily found in both Windows & Mac system
         pic_pathSTR = pic_pathSTR.replace('\\', "/", 30)
         #pic_typeSTR = pic_infoLIST[1]
-        
+        print(pic_pathSTR)
         
         # Rename the file
         # src: The source file path (the existing file to be renamed).
@@ -51,42 +101,42 @@ if __name__ == "__main__":
         pic_infoLIST[6] = emotion  e.g. angry & fearful & neutral   (A/F/M)
         """
         
-        ## Regroup the pics based on the emotions
-        # Determine the emotion category
-        if pic_infoLIST[6] == "angry":
-            sub_emoSTR = "A"
-        if pic_infoLIST[6] == "fearful":
-            sub_emoSTR = "F"
-        if pic_infoLIST[6] == "neutral":
-            sub_emoSTR = "N"
-        else:
-            pass
+        #print(pic_infoLIST[4])
+        #print(pic_infoLIST[5])
+        #print(pic_infoLIST[6])
         
-        # Determine the gender category
-        if pic_infoLIST[4] == "female":
-            sub_genderSTR = "F"
-        if pic_infoLIST[4] == "male":
-            sub_genderSTR = "M"
-        else:
-            pass
+        count_FB_INT = 0
+        count_FW_INT = 0
+        count_FA_INT = 0
+        count_MB_INT = 0
+        count_MW_INT = 0
+        count_MA_INT = 0
+        genderSTR = gender(pic_infoLIST[4])
+        raceSTR = race(pic_infoLIST[5])
+        emotionSTR = emotion(pic_infoLIST[6])
         
-        # Determine the race category
-        if pic_infoLIST[5] == "black":
-            sub_raceSTR = "B"
-        if pic_infoLIST[5] == "caucasion":
-            sub_raceSTR = "C"
-        if pic_infoLIST[5] == "asion":
-            sub_raceSTR = "A"
-        else:
-            pass
-            
-            
-        new_picnameSTR = "%s_%s%s_%d" %(sub_emoSTR, sub_genderSTR, sub_raceSTR, "loop_num") # put in the real loop_num first
+        tmpSTR = raceSTR+genderSTR
+        if tmpSTR == "FB":
+            count_FB_INT=+1
+        if tmpSTR == "FB":
+            count_FB_INT=+1
+        if tmpSTR == "FB":
+                count_FB_INT=+1
         
+        
+        #tmp_picnameSTR = "%s_%s%s" %(emotionSTR, genderSTR, raceSTR) #, count_A_INT) # put in the real loop_num first
+        #print(tmp_picnameSTR)
+
+        
+        #print(new_picnameSTR)
+        
+        """
         # Put the pics into different folder based on emotion types
         if sub_emoSTR == "A":
-            n_picFolder_path = root_data_path / "faces" / Path("Angry_emotion" %(sub_id, sub_cond))
+            n_picFolder_path = root_data_path / "faces" / Path("Angry_face" %(sub_id, sub_cond))
             n_picFolder_path.mkdir(exist_ok=True)
             src = face_data_path  # old file name
             dst = n_picFolder_path # new file name
             os.rename(src, dst, src_dir_fd=None, dst_dir_fd=None)
+            
+            """
